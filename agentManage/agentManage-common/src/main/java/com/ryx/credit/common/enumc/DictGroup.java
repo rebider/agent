cdca699fb2d64fc5510fa9c3ebd7abc942363255
cx <chenxiaoxiang@ruiyanxinde.com>
2018-05-22 17:23:18 +0800

    代理商及合同服务

--- agentManage/agentManage-common/src/main/java/com/ryx/credit/common/enumc/DictGroup.java
+++ agentManage/agentManage-common/src/main/java/com/ryx/credit/common/enumc/DictGroup.java
@@ -8,7 +8,7 @@ import java.math.BigDecimal;
 public enum DictGroup {
 
 
-    AGENT("代理商"),CAPITAL_TYPE("交款类型"),CONTRACT_TYPE("合同类型");
+    AGENT("代理商模块"),CAPITAL_TYPE("交款类型"),CONTRACT_TYPE("合同类型");
 
 
     public String  msg;
