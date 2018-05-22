63c30323c2ff4e726916a8aea7cd2138f04e6a24
cx <chenxiaoxiang@ruiyanxinde.com>
2018-05-22 16:11:23 +0800

    选项查询服务 代理商服务

--- agentManage/agentManage-common/src/main/java/com/ryx/credit/common/enumc/Status.java
+++ agentManage/agentManage-common/src/main/java/com/ryx/credit/common/enumc/Status.java
@@ -5,13 +5,13 @@ import java.math.BigDecimal;
 /**
  * Created by cx on 2018/5/22.
  */
-public enum STATUS {
+public enum Status {
 
     STATUS_0(0),STATUS_1(1);
 
     public BigDecimal status;
 
-    STATUS(int s){
+    Status(int s){
         status = new BigDecimal(s);
     }
 
