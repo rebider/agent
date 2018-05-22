389838dd592bf1790e14a9125f17e4e5848d0fb2
cx <chenxiaoxiang@ruiyanxinde.com>
2018-05-22 15:35:56 +0800

    选项查询服务

---
+++ agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/service/impl/dict/DictOptionsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.ryx.credit.service.impl.dict;
+
+import com.ryx.credit.common.enumc.STATUS;
+import com.ryx.credit.dao.agent.DictMapper;
+import com.ryx.credit.pojo.admin.agent.Dict;
+import com.ryx.credit.pojo.admin.agent.DictExample;
+import com.ryx.credit.service.dict.DictOptionsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 选项服务类
+ * Created by cx on 2018/5/22.
+ */
+@Service
+public class DictOptionsServiceImpl implements DictOptionsService {
+
+
+    @Autowired
+    private DictMapper dictMapper;
+
+    @Override
+    public List<Dict> dictList(String group, String artifact) {
+        DictExample example  = new DictExample();
+        example.or().andDGroupEqualTo(group).andDArtifactEqualTo(artifact).andDStatusEqualTo(STATUS.STATUS_1.status);
+        example.setOrderByClause(" D_sort desc");
+        return  dictMapper.selectByExample(example);
+    }
+}
--- agentManage/agentManageAdmin-dubbo/src/test/java/DictServiceTest.java
+++ agentManage/agentManageAdmin-dubbo/src/test/java/DictServiceTest.java
@@ -1,24 +1,42 @@
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.annotations.TableId;
 import com.ryx.credit.common.enumc.TabId;
+import com.ryx.credit.pojo.admin.agent.Dict;
+import com.ryx.credit.service.dict.DictOptionsService;
 import com.ryx.credit.service.dict.IdService;
 import org.junit.Assert;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 
 /**
  * Created by cx on 2018/5/22.
  */
 public class DictServiceTest extends BaseSpringTest {
 
+    private Logger  logger = LoggerFactory.getLogger(DictServiceTest.class);
 
     @Autowired
     private IdService idService;
+    @Autowired
+    private DictOptionsService dictOptionsService;
 
     @Test
     public void testId(){
-        System.out.println("=======testId=====");
-        System.out.println("======="+idService.genId(TabId.a_agent));
-        System.out.println("=======testId=====");
+        logger.info("=======testId=====");
+        logger.info("======="+idService.genId(TabId.a_agent));
+        logger.info("=======testId=====");
+    }
+
+    @Test
+    public void testOptions(){
+        logger.info("=======testOptions=====");
+        List<Dict> list =  dictOptionsService.dictList("AGENT","CAPITAL_TYPE");
+        logger.info("=======testOptions====="+ JSONObject.toJSONString(list));
     }
 
 }
