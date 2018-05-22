b04c1cfa80a1b69f6da93a6b92e27704bd828269
liudh <liudonghui-bj@ruiyinxin.com>
2018-05-22 09:29:24 +0800

    service初始化

---
+++ agentManage/agentManageAdmin-api/src/main/java/com/ryx/credit/service/agent/BusinessPlatformService.java
@@ -0,0 +1,12 @@
+package com.ryx.credit.service.agent;
+
+/**
+ * 业务平台管理
+ * @version V1.0
+ * @Description:
+ * @author: Liudh
+ * @date: 2018/5/22 9:27
+ */
+public interface BusinessPlatformService {
+
+}
---
+++ agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/service/impl/agent/BusinessPlatformServiceImpl.java
@@ -0,0 +1,18 @@
+package com.ryx.credit.service.impl.agent;
+
+import com.ryx.credit.service.agent.BusinessPlatformService;
+import org.springframework.stereotype.Service;
+
+/**
+ * 业务平台管理
+ * @version V1.0
+ * @Description:
+ * @author: Liudh
+ * @date: 2018/5/22 9:26
+ */
+@Service("businessPlatformService")
+public class BusinessPlatformServiceImpl implements BusinessPlatformService {
+
+
+
+}
