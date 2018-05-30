c612ab3b5d768882950c4433bae27ae38946a9a2
liudh <liudonghui-bj@ruiyinxin.com>
2018-05-30 16:01:04 +0800

    入网审核

--- agentManage/agentManage-common/src/main/java/com/ryx/credit/common/enumc/DictGroup.java
+++ agentManage/agentManage-common/src/main/java/com/ryx/credit/common/enumc/DictGroup.java
@@ -7,8 +7,17 @@ import java.math.BigDecimal;
  */
 public enum DictGroup {
 
-
-    ALL("所有模块"),YESORNO("yesorno"), AGENT("代理商模块"),BUS_TYPE("业务模块类型或者级别"),CAPITAL_TYPE("交款类型"),CONTRACT_TYPE("合同类型"),CERT_TYPE("证件类型"),AGNATURE_TYPE("公司类型"),COLINFO_TYPE("收款账户类型");
+    ALL("所有模块"),
+    YESORNO("yesorno"),
+    AGENT("代理商模块"),
+    AGENT_AUDIT("代理商审核模块"),
+    BUS_TYPE("业务模块类型或者级别"),
+    CAPITAL_TYPE("交款类型"),
+    CONTRACT_TYPE("合同类型"),
+    CERT_TYPE("证件类型"),
+    AGNATURE_TYPE("公司类型"),
+    COLINFO_TYPE("收款账户类型"),
+    APPROVAL_TYPE("审批结果类型");
 
 
     public String  msg;
