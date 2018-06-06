c53dca6bf799cee0559dc71d79c5f61fdb91d2de
cx <chenxiaoxiang@ruiyanxinde.com>
2018-06-06 18:09:48 +0800

    代理商入网,审批通过

--- agentManage/agentManageActivity-dubbo/src/main/java/com/ryx/credit/spring/MySpringContextHandler.java
+++ agentManage/agentManageActivity-dubbo/src/main/java/com/ryx/credit/spring/MySpringContextHandler.java
@@ -1,8 +1,12 @@
 package com.ryx.credit.spring;
 
+import com.ryx.credit.service.agent.AgentEnterService;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+
+import javax.annotation.PostConstruct;
 
 /**
  * Created by cx on 2018/5/31.
@@ -12,8 +16,25 @@ public class MySpringContextHandler implements ApplicationContextAware {
 
     public static ApplicationContext applicationContext;
 
+
+    @PostConstruct
+    public void init(){
+        applicationContext.getBean("mySpringContextHandler");
+    }
+
+    @Autowired
+    private AgentEnterService agentEnterService;
+
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         this.applicationContext = applicationContext;
+    }
+
+    public AgentEnterService getAgentEnterService() {
+        return agentEnterService;
+    }
+
+    public void setAgentEnterService(AgentEnterService agentEnterService) {
+        this.agentEnterService = agentEnterService;
     }
 }
