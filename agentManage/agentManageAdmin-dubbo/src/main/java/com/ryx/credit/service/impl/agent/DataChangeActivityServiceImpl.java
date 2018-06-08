790725357f411465ca16695e745c203f73eaf781
cx <chenxiaoxiang@ruiyanxinde.com>
2018-06-08 14:15:30 +0800

    代理商入网,业务修改审批

--- agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/service/impl/agent/DataChangeActivityServiceImpl.java
+++ agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/service/impl/agent/DataChangeActivityServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ryx.credit.service.impl.agent;
 
+import com.alibaba.fastjson.JSONObject;
 import com.ryx.credit.common.enumc.*;
 import com.ryx.credit.common.exception.ProcessException;
 import com.ryx.credit.common.util.ResultVO;
@@ -10,6 +11,7 @@ import com.ryx.credit.pojo.admin.agent.BusActRel;
 import com.ryx.credit.pojo.admin.agent.BusActRelExample;
 import com.ryx.credit.pojo.admin.agent.DateChangeRequest;
 import com.ryx.credit.pojo.admin.agent.Dict;
+import com.ryx.credit.pojo.admin.vo.AgentVo;
 import com.ryx.credit.service.ActivityService;
 import com.ryx.credit.service.agent.*;
 import com.ryx.credit.service.dict.DictOptionsService;
@@ -52,11 +54,14 @@ public class DataChangeActivityServiceImpl implements DataChangeActivityService 
     private BusActRelMapper busActRelMapper;
     @Autowired
     private DictOptionsService dictOptionsService;
+    @Autowired
+    private AgentEnterService agentEnterService;
+
 
     @Transactional(isolation = Isolation.DEFAULT,propagation = Propagation.REQUIRED,rollbackFor = Exception.class)
     @Override
     public ResultVO startDataChangeActivity(String dataChangeId,String userId) {
-        logger.info("========用户{}启动数据修改申请{}",dataChangeId,userId);
+        logger.info("========用户{}启动数据修改申请{}",userId,dataChangeId);
         DateChangeRequest dateChangeRequest = dateChangeRequestMapper.selectByPrimaryKey(dataChangeId);
 
         BusActRelExample example = new BusActRelExample();
@@ -105,5 +110,86 @@ public class DataChangeActivityServiceImpl implements DataChangeActivityService 
         }
 
         return ResultVO.success(null);
+    }
+
+
+    /**
+     * 收款账户修改 审批完成处理
+     * @param proIns
+     * @param agStatus
+     * @return
+     */
+    @Override
+    public ResultVO compressColInfoDataChangeActivity(String proIns, String agStatus) {
+        try {
+            BusActRelExample example = new BusActRelExample();
+            example.or().andStatusEqualTo(Status.STATUS_1.status).andActivIdEqualTo(proIns).andActivStatusEqualTo(AgStatus.Approving.name());
+            List<BusActRel> resl =  busActRelMapper.selectByExample(example);
+            if(resl.size()==0){
+                logger.info("========审批流完成{}状态{}未找到数据与审批流关系",proIns,agStatus);
+                return ResultVO.fail("未找到数据与审批流关系");
+            }
+            BusActRel rel = resl.get(0);
+            logger.info("========审批流完成{}业务{}状态{}",proIns,rel.getBusType(),agStatus);
+            DateChangeRequest dr = dateChangeRequestMapper.selectByPrimaryKey(rel.getBusId());
+            if(dr==null){
+                logger.info("========审批流完成{}业务{}状态{} 未找到数据",proIns,rel.getBusType(),agStatus);
+                throw new ProcessException("更新数据申请失败");
+            }
+            try {
+                if(AgStatus.Approved.name().equals(agStatus)){
+                    //收款账户修改
+                    if(DataChangeApyType.DC_Colinfo.name().equals(dr.getDataType())) {
+                        //更新入库
+                        AgentVo vo = JSONObject.parseObject(dr.getDataContent(), AgentVo.class);
+                        ResultVO res = agentColinfoService.updateAgentColinfoVo(vo.getColinfoVoList(), vo.getAgent());
+                        logger.info("========审批流完成{}业务{}状态{},结果{}", proIns, rel.getBusType(), agStatus, res.getResInfo());
+                        //更新数据状态为审批成功
+                        if(res.isSuccess()){
+                            dr.setAppyStatus(AgStatus.Approved.status);
+                            dr.setcUpdate(Calendar.getInstance().getTime());
+                            logger.info("========审批流完成{}业务{}状态{},结果{}",proIns,rel.getBusType(),agStatus,"更新数据申请失败");
+                            if(1!=dateChangeRequestMapper.updateByPrimaryKeySelective(dr)){
+                                throw new ProcessException("更新数据申请失败");
+                            }
+                        }
+                    //代理商新修改
+                    }else if(DataChangeApyType.DC_Agent.name().equals(dr.getDataType())){
+                        //更新入库
+                        AgentVo vo = JSONObject.parseObject(dr.getDataContent(), AgentVo.class);
+                        ResultVO res = agentEnterService.updateAgentVo(vo,rel.getcUser());
+                        logger.info("========审批流完成{}业务{}状态{},结果{}", proIns, rel.getBusType(), agStatus, res.getResInfo());
+                        //更新数据状态为审批成功
+                        if(res.isSuccess()){
+                            dr.setAppyStatus(AgStatus.Approved.status);
+                            dr.setcUpdate(Calendar.getInstance().getTime());
+                            logger.info("========审批流完成{}业务{}状态{},结果{}",proIns,rel.getBusType(),agStatus,"更新数据申请失败");
+                            if(1!=dateChangeRequestMapper.updateByPrimaryKeySelective(dr)){
+                                throw new ProcessException("更新数据申请失败");
+                            }
+                        }
+                    }
+                //拒绝更新数据状态
+                }else if(AgStatus.Refuse.name().equals(agStatus)){
+                    dr.setAppyStatus(AgStatus.Refuse.status);
+                    dr.setcUpdate(Calendar.getInstance().getTime());
+                    logger.info("========审批流完成{}业务{}状态{},结果{}",proIns,rel.getBusType(),agStatus,"更新数据申请失败");
+                    if(1!=dateChangeRequestMapper.updateByPrimaryKeySelective(dr)){
+                        throw new ProcessException("更新数据申请失败");
+                    }
+                }
+                rel.setActivStatus(agStatus);
+                if(1!=busActRelMapper.updateByPrimaryKeySelective(rel)){
+                    throw new ProcessException("更新数据申请失败");
+                }
+            } catch (ProcessException e) {
+                e.printStackTrace();
+                return ResultVO.fail(e.getMessage());
+            }
+            return ResultVO.success(dr);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResultVO.fail(e.getMessage());
+        }
     }
 }
