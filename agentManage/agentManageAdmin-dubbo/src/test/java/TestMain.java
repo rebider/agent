35e41073964b815cc8ee89782395051f6a70c228
cx <chenxiaoxiang@ruiyanxinde.com>
2018-06-13 11:41:11 +0800

    测试

---
+++ agentManage/agentManageAdmin-dubbo/src/test/java/TestMain.java
@@ -0,0 +1,11 @@
+import com.ryx.credit.service.impl.agent.AimportServiceImpl;
+
+/**
+ * Created by cx on 2018/6/12.
+ */
+public class TestMain {
+
+    public static void main(String[] args) {
+        System.out.println(AimportServiceImpl.gs.indexOf("对私"));
+    }
+}
