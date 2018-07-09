fc670e42fe6c68ba87541699d1c01bc892e84a36
liudh <liudonghui@ruiyinxin.com>
2018-07-09 10:03:25 +0800

    增加是否附加协议字段

--- agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/service/impl/agent/AgentContractServiceImpl.java
+++ agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/service/impl/agent/AgentContractServiceImpl.java
@@ -183,6 +183,7 @@ public class AgentContractServiceImpl implements AgentContractService {
                     db_AgentContract.setRemark(agentContractVo.getRemark());
                     db_AgentContract.setcUser(agentContractVo.getcUser());
                     db_AgentContract.setStatus(agentContractVo.getStatus());
+                    db_AgentContract.setAppendAgree(agentContractVo.getAppendAgree());
                     if (1 != agentContractMapper.updateByPrimaryKeySelective(db_AgentContract)) {
                         throw new ProcessException("更新收款信息失败");
                     }
