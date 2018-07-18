2aa40c949a6058ee71cd46993c8669233c6b20ca
cx <chenxiaoxiang@ruiyanxinde.com>
2018-07-18 21:09:54 +0800

    二期订单

---
+++ agentManage/agentManageAdmin-api/src/main/java/com/ryx/credit/pojo/admin/vo/OrderFormVo.java
@@ -0,0 +1,13 @@
+package com.ryx.credit.pojo.admin.vo;
+
+import com.ryx.credit.pojo.admin.order.OOrder;
+
+/**
+ * Created by RYX on 2018/7/18.
+ * 订单表单表
+ */
+public class OrderFormVo extends OOrder{
+
+
+
+}
