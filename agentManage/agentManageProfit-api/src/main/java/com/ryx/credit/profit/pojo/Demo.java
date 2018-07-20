d96e29488c511175f9681159d87b3e49ee7274b8
wangqi <wangqi-bj@ruiyinxin.com>
2018-07-20 14:42:37 +0800

    update: profit module

---
+++ agentManage/agentManageProfit-api/src/main/java/com/ryx/credit/profit/pojo/Demo.java
@@ -0,0 +1,14 @@
+package com.ryx.credit.profit.pojo;
+
+/**
+ * Demo
+ * Created by IntelliJ IDEA.
+ *
+ * @author Wang Qi
+ * @version 1.0 2018/7/20 14:42
+ * @see Demo
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class Demo {
+}
