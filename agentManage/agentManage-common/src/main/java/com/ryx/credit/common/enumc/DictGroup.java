36f26d7a598c240704353c9b06c60ac94e70c4b6
ymingxu <249952285@qq.com>
2018-07-20 14:03:14 +0800

    1、模板协议类型增加枚举

--- agentManage/agentManage-common/src/main/java/com/ryx/credit/common/enumc/DictGroup.java
+++ agentManage/agentManage-common/src/main/java/com/ryx/credit/common/enumc/DictGroup.java
@@ -33,7 +33,9 @@ public enum DictGroup {
     BUS_SCOPE("业务范围"),
     //订单
     MODEL_TYPE("机具类型"),
-    SETTLEMENT_TYPE("结算类型");
+    SETTLEMENT_TYPE("结算类型"),
+    TEMP_AGREE("协议模板"),
+    AGREE_TYPE("协议类型");
 
     public String  msg;
 
