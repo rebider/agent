162c9d061bcd8621375efa500f6670c3f777bfa3
ymingxu <249952285@qq.com>
2018-07-24 17:46:32 +0800

    1、创建月分润数据展示服务接口与实现

--- agentManage/agentManageProfit-dubbo/src/main/java/com/ryx/credit/profit/service/impl/ProfitMonthServiceImpl.java
+++ agentManage/agentManageProfit-dubbo/src/main/java/com/ryx/credit/profit/service/impl/ProfitMonthServiceImpl.java
@@ -1,9 +1,16 @@
 package com.ryx.credit.profit.service.impl;
 
 import com.ryx.credit.common.util.Page;
+import com.ryx.credit.commons.utils.StringUtils;
+import com.ryx.credit.profit.dao.ProfitDetailMonthMapper;
+import com.ryx.credit.profit.dao.ProfitMonthMapper;
 import com.ryx.credit.profit.pojo.ProfitDetailMonth;
+import com.ryx.credit.profit.pojo.ProfitDetailMonthExample;
 import com.ryx.credit.profit.pojo.ProfitMonth;
+import com.ryx.credit.profit.pojo.ProfitMonthExample;
 import com.ryx.credit.profit.service.ProfitMonthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -11,29 +18,58 @@ import java.util.List;
  * @author yangmx
  * @desc 月分润数据展示服务实现
  */
+@Service("profitMonthService")
 public class ProfitMonthServiceImpl implements ProfitMonthService {
+
+    @Autowired
+    private ProfitMonthMapper profitMonthMapper;
+    @Autowired
+    private ProfitDetailMonthMapper profitDetailMonthMapper;
 
     @Override
     public List<ProfitMonth> getProfitMonthList(Page page, ProfitMonth profitMonth) {
-
-        return null;
+        ProfitMonthExample profitMonthExample= this.profitMonthEqualsTo(profitMonth);
+        profitMonthExample.setPage(page);
+        return profitMonthMapper.selectByExample(profitMonthExample);
     }
 
-    @Override
-    public Long getProfitMonthCount(ProfitMonth profitMonth) {
+    private ProfitMonthExample profitMonthEqualsTo(ProfitMonth profitMonth) {
+        ProfitMonthExample profitMonthExample = new ProfitMonthExample();
+        ProfitMonthExample.Criteria criteria = profitMonthExample.createCriteria();
+        if(StringUtils.isNotBlank(profitMonth.getAgentName())){
+            criteria.andAgentNameEqualTo(profitMonth.getAgentName());
+        }
+        if(StringUtils.isNotBlank(profitMonth.getAgentId())){
+            criteria.andAgentIdEqualTo(profitMonth.getAgentId());
+        }
+        if(StringUtils.isNotBlank(profitMonth.getProfitDate())){
+            criteria.andProfitDateEqualTo(profitMonth.getProfitDate());
+        }
+        if(StringUtils.isNotBlank(profitMonth.getStatus())){
+            criteria.andStatusEqualTo(profitMonth.getStatus());
+        }
+        return profitMonthExample;
+    }
 
-        return null;
+
+    @Override
+    public int getProfitMonthCount(ProfitMonth profitMonth) {
+        ProfitMonthExample profitMonthExample= this.profitMonthEqualsTo(profitMonth);
+        return profitMonthMapper.countByExample(profitMonthExample);
     }
 
     @Override
     public List<ProfitDetailMonth> getProfitDetailMonthList(Page page, ProfitDetailMonth profitDetailMonth) {
 
-        return null;
+        ProfitDetailMonthExample profitDetailMonthExample = new ProfitDetailMonthExample();
+        ProfitDetailMonthExample.Criteria criteria = profitDetailMonthExample.createCriteria();
+        profitDetailMonthExample.setPage(page);
+        return profitDetailMonthMapper.selectByExample(profitDetailMonthExample);
     }
 
     @Override
-    public Long getProfitDetailMonthCount(ProfitDetailMonth profitDetailMonth) {
+    public int getProfitDetailMonthCount(ProfitDetailMonth profitDetailMonth) {
 
-        return null;
+        return 0;
     }
 }
