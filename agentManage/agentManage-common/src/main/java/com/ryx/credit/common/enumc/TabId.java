3a15651b56367ad9a38b6e56349dfd7e06c2bc84
wangyong-bj <wangyong-bj@ruiyinxin.com>
2018-07-27 17:20:33 +0800

    分润调整序列

--- agentManage/agentManage-common/src/main/java/com/ryx/credit/common/enumc/TabId.java
+++ agentManage/agentManage-common/src/main/java/com/ryx/credit/common/enumc/TabId.java
@@ -6,6 +6,7 @@ package com.ryx.credit.common.enumc;
  */
 public enum TabId {
 
+
     a_agent("AG%s%015d"),
     a_agent_colinfo("AC%s%015d"),
     a_agent_contract("AO%s%015d"),
@@ -42,7 +43,8 @@ public enum TabId {
     P_DEDUCTION("P_DEDUCTION%s%015d"),
     p_profir_detail("P_PROFIT_DETAIL_M%s%015d"),
     p_profir_m("P_PROFIT_M%s%015d"),
-    p_profit_unfreeze("P_PROFIT_UNFREEZE%s%015d");
+    p_profit_unfreeze("P_PROFIT_UNFREEZE%s%015d"),
+    p_profit_adjust("P_PA%s%015d");
 
     TabId(String thePatt){
            this.patt = thePatt;
