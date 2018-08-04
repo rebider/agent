bb2eaaf1bc902be48062118d70a6156f8edb99e1
wangyong-bj <wangyong-bj@ruiyinxin.com>
2018-08-04 10:52:36 +0800

    xulie

--- agentManage/agentManage-common/src/main/java/com/ryx/credit/common/enumc/TabId.java
+++ agentManage/agentManage-common/src/main/java/com/ryx/credit/common/enumc/TabId.java
@@ -56,7 +56,11 @@ public enum TabId {
     p_profir_m("PRM%s%015d"),
     p_profit_unfreeze("UNF%s%015d"),
     p_profit_adjust("PPA%s%015d"),
-    p_profit_factor("PF%s%015d");
+    p_profit_factor("PF%s%015d"),
+    p_tax_adjust("PTA%s%015d"),
+    p_pos_reward("PPR%s%015d"),
+    p_pos_check("PPC%s%015d"),
+    p_profit_supply("PPS%s%015d");
 
     TabId(String thePatt){
            this.patt = thePatt;
