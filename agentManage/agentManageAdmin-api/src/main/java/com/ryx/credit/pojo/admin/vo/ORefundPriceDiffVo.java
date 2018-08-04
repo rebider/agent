1670b258b27f6ecb58ded7ff79ce0f762d02a279
liudh <liudonghui@ruiyinxin.com>
2018-08-04 14:46:54 +0800

    退补差价查询列表优化

--- agentManage/agentManageAdmin-api/src/main/java/com/ryx/credit/pojo/admin/vo/ORefundPriceDiffVo.java
+++ agentManage/agentManageAdmin-api/src/main/java/com/ryx/credit/pojo/admin/vo/ORefundPriceDiffVo.java
@@ -8,11 +8,31 @@ public class ORefundPriceDiffVo extends ORefundPriceDiff implements Serializable
 
     private String gatherTimeStr;
 
+    private String applyBeginTime;
+
+    private String applyEndTime;
+
     public String getGatherTimeStr() {
         return gatherTimeStr;
     }
 
     public void setGatherTimeStr(String gatherTimeStr) {
         this.gatherTimeStr = gatherTimeStr;
+    }
+
+    public String getApplyBeginTime() {
+        return applyBeginTime;
+    }
+
+    public void setApplyBeginTime(String applyBeginTime) {
+        this.applyBeginTime = applyBeginTime;
+    }
+
+    public String getApplyEndTime() {
+        return applyEndTime;
+    }
+
+    public void setApplyEndTime(String applyEndTime) {
+        this.applyEndTime = applyEndTime;
     }
 }
--- agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/service/impl/order/CompensateServiceImpl.java
+++ agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/service/impl/order/CompensateServiceImpl.java
@@ -85,11 +85,19 @@ public class CompensateServiceImpl implements CompensateService {
 
 
     @Override
-    public PageInfo compensateList(ORefundPriceDiff refundPriceDiff, Page page){
+    public PageInfo compensateList(ORefundPriceDiffVo refundPriceDiff, Page page){
 
         ORefundPriceDiffExample example = new ORefundPriceDiffExample();
         ORefundPriceDiffExample.Criteria criteria = example.createCriteria();
-
+        if(StringUtils.isNotBlank(refundPriceDiff.getApplyBeginTime())){
+            criteria.andSTimeGreaterThanOrEqualTo(DateUtil.getDateFromStr(refundPriceDiff.getApplyBeginTime(),DateUtil.DATE_FORMAT_1));
+        }
+        if(StringUtils.isNotBlank(refundPriceDiff.getApplyEndTime())){
+            criteria.andSTimeLessThanOrEqualTo(DateUtil.getDateFromStr(refundPriceDiff.getApplyEndTime(),DateUtil.DATE_FORMAT_1));
+        }
+        if(null!=refundPriceDiff.getReviewStatus()){
+            criteria.andReviewStatusEqualTo(refundPriceDiff.getReviewStatus());
+        }
         example.setPage(page);
         example.setOrderByClause("c_time desc");
         List<ORefundPriceDiff> refundPriceDiffs = refundPriceDiffMapper.selectByExample(example);
@@ -541,6 +549,9 @@ public class CompensateServiceImpl implements CompensateService {
         //查询关联附件
         List<Attachment> attachments = attachmentMapper.accessoryQuery(oRefundPriceDiff.getId(), AttachmentRelType.ActivityEdit.name());
         oRefundPriceDiff.setAttachmentList(attachments);
+        //查询财务打款关联附件
+        List<Attachment> attachmentFianceList = attachmentMapper.accessoryQuery(oRefundPriceDiff.getId(), AttachmentRelType.ActivityFinanceEdit.name());
+        oRefundPriceDiff.setAttachmentFianceList(attachmentFianceList);
         //查询扣除款项
         ODeductCapitalExample oDeductCapitalExample = new ODeductCapitalExample();
         ODeductCapitalExample.Criteria criteria2 = oDeductCapitalExample.createCriteria();
