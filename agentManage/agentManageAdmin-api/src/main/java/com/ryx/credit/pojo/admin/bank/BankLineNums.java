3906c7ccba874513900d4ba6324d5450e5972fd0
liudh <liudonghui@ruiyinxin.com>
2018-08-31 11:34:23 +0800

    联行号重构

---
+++ agentManage/agentManageAdmin-api/src/main/java/com/ryx/credit/pojo/admin/bank/BankLineNums.java
@@ -0,0 +1,203 @@
+package com.ryx.credit.pojo.admin.bank;
+
+public class BankLineNums {
+    private String banktype;
+
+    private String provinceid;
+
+    private String provincename;
+
+    private String cityid;
+
+    private String cityname;
+
+    private String bankid;
+
+    private String bankname;
+
+    private String banknameshort;
+
+    private String bankbranchid;
+
+    private String bankbranchname;
+
+    private String bankaddress;
+
+    private String liqbankid;
+
+    private String liqbankcityid;
+
+    private String flag;
+
+    private String bankcityid;
+
+    private String bankpostcode;
+
+    private String telnum;
+
+    private String setupdate;
+
+    private String upbankid;
+
+    private String headbankid;
+
+    public String getBanktype() {
+        return banktype;
+    }
+
+    public void setBanktype(String banktype) {
+        this.banktype = banktype == null ? null : banktype.trim();
+    }
+
+    public String getProvinceid() {
+        return provinceid;
+    }
+
+    public void setProvinceid(String provinceid) {
+        this.provinceid = provinceid == null ? null : provinceid.trim();
+    }
+
+    public String getProvincename() {
+        return provincename;
+    }
+
+    public void setProvincename(String provincename) {
+        this.provincename = provincename == null ? null : provincename.trim();
+    }
+
+    public String getCityid() {
+        return cityid;
+    }
+
+    public void setCityid(String cityid) {
+        this.cityid = cityid == null ? null : cityid.trim();
+    }
+
+    public String getCityname() {
+        return cityname;
+    }
+
+    public void setCityname(String cityname) {
+        this.cityname = cityname == null ? null : cityname.trim();
+    }
+
+    public String getBankid() {
+        return bankid;
+    }
+
+    public void setBankid(String bankid) {
+        this.bankid = bankid == null ? null : bankid.trim();
+    }
+
+    public String getBankname() {
+        return bankname;
+    }
+
+    public void setBankname(String bankname) {
+        this.bankname = bankname == null ? null : bankname.trim();
+    }
+
+    public String getBanknameshort() {
+        return banknameshort;
+    }
+
+    public void setBanknameshort(String banknameshort) {
+        this.banknameshort = banknameshort == null ? null : banknameshort.trim();
+    }
+
+    public String getBankbranchid() {
+        return bankbranchid;
+    }
+
+    public void setBankbranchid(String bankbranchid) {
+        this.bankbranchid = bankbranchid == null ? null : bankbranchid.trim();
+    }
+
+    public String getBankbranchname() {
+        return bankbranchname;
+    }
+
+    public void setBankbranchname(String bankbranchname) {
+        this.bankbranchname = bankbranchname == null ? null : bankbranchname.trim();
+    }
+
+    public String getBankaddress() {
+        return bankaddress;
+    }
+
+    public void setBankaddress(String bankaddress) {
+        this.bankaddress = bankaddress == null ? null : bankaddress.trim();
+    }
+
+    public String getLiqbankid() {
+        return liqbankid;
+    }
+
+    public void setLiqbankid(String liqbankid) {
+        this.liqbankid = liqbankid == null ? null : liqbankid.trim();
+    }
+
+    public String getLiqbankcityid() {
+        return liqbankcityid;
+    }
+
+    public void setLiqbankcityid(String liqbankcityid) {
+        this.liqbankcityid = liqbankcityid == null ? null : liqbankcityid.trim();
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String flag) {
+        this.flag = flag == null ? null : flag.trim();
+    }
+
+    public String getBankcityid() {
+        return bankcityid;
+    }
+
+    public void setBankcityid(String bankcityid) {
+        this.bankcityid = bankcityid == null ? null : bankcityid.trim();
+    }
+
+    public String getBankpostcode() {
+        return bankpostcode;
+    }
+
+    public void setBankpostcode(String bankpostcode) {
+        this.bankpostcode = bankpostcode == null ? null : bankpostcode.trim();
+    }
+
+    public String getTelnum() {
+        return telnum;
+    }
+
+    public void setTelnum(String telnum) {
+        this.telnum = telnum == null ? null : telnum.trim();
+    }
+
+    public String getSetupdate() {
+        return setupdate;
+    }
+
+    public void setSetupdate(String setupdate) {
+        this.setupdate = setupdate == null ? null : setupdate.trim();
+    }
+
+    public String getUpbankid() {
+        return upbankid;
+    }
+
+    public void setUpbankid(String upbankid) {
+        this.upbankid = upbankid == null ? null : upbankid.trim();
+    }
+
+    public String getHeadbankid() {
+        return headbankid;
+    }
+
+    public void setHeadbankid(String headbankid) {
+        this.headbankid = headbankid == null ? null : headbankid.trim();
+    }
+}
