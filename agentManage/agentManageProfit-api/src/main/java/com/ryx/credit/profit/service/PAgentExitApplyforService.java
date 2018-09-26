68bfef4d3dd867cb2a8f931f248967b32fdc3756
lihaili <lihaili-bj@ruiyinxin.com>
2018-09-26 16:40:57 +0800

    service

---
+++ agentManage/agentManageProfit-api/src/main/java/com/ryx/credit/profit/service/PAgentExitApplyforService.java
@@ -0,0 +1,19 @@
+package com.ryx.credit.profit.service;
+
+import com.ryx.credit.common.exception.ProcessException;
+import com.ryx.credit.common.result.AgentResult;
+import com.ryx.credit.common.util.Page;
+import com.ryx.credit.common.util.PageInfo;
+import com.ryx.credit.pojo.admin.vo.AgentVo;
+import com.ryx.credit.profit.pojo.PAgentExitApplyfor;
+
+import java.util.List;
+
+/**
+ * @Author Lihl
+ * @Date 2018/9/26
+ * @Desc 代理商退出申请
+ */
+public interface PAgentExitApplyforService {
+
+}
