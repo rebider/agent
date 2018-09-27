1570b769dcc87b1b13cc2bab8e10a160fc05bd75
liuqianyuan-b <liuqianyuan-bj@ruiyinxin.com>
2018-09-27 11:23:25 +0800

    代理商合并Service

---
+++ agentManage/agentManageProfit-api/src/main/java/com/ryx/credit/profit/service/IProfitAgentMergerService.java
@@ -0,0 +1,10 @@
+package com.ryx.credit.profit.service;
+/**
+ * 代理商合并Service
+ * @version V1.0
+ * @Description:
+ * @author: LiuQY
+ * @date: 2018/9/27 09:30
+ */
+public interface IProfitAgentMergerService {
+}
