85b3b5f3532e032c5bd34bc46df28373b474e5ec
liudh <liudonghui@ruiyinxin.com>
2018-10-09 14:28:17 +0800

    机构统计信息查询

---
+++ agentManage/agentManageAdmin-api/src/main/java/com/ryx/credit/service/agent/PosOrgStatisticsService.java
@@ -0,0 +1,12 @@
+package com.ryx.credit.service.agent;
+
+import com.ryx.credit.common.result.AgentResult;
+
+/**
+ * Created by RYX on 2018/10/9.
+ */
+public interface PosOrgStatisticsService {
+
+    AgentResult posOrgStatistics(String orgId)throws Exception;
+    
+}
