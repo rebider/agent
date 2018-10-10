0b26b90ff193b4445008572a91d1b24a0661aa01
cx <chenxiaoxiang@ruiyanxinde.com>
2018-10-10 14:47:36 +0800

    POS终端机具查询

--- agentManage/agentManageMachine-dubbo/src/main/java/com/ryx/credit/machine/service/impl/PosTermMachineServiceImpl.java
+++ agentManage/agentManageMachine-dubbo/src/main/java/com/ryx/credit/machine/service/impl/PosTermMachineServiceImpl.java
@@ -2,12 +2,15 @@ package com.ryx.credit.machine.service.impl;
 
 import com.alibaba.fastjson.JSONObject;
 import com.ryx.credit.common.enumc.PlatformType;
+import com.ryx.credit.machine.entity.ImsTermMachine;
+import com.ryx.credit.machine.service.ImsTermMachineService;
 import com.ryx.credit.machine.service.TermMachineService;
 import com.ryx.credit.machine.vo.MposTermBatchVo;
 import com.ryx.credit.machine.vo.MposTermTypeVo;
 import com.ryx.credit.machine.vo.TermMachineVo;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -20,9 +23,20 @@ import java.util.Map;
 @Service("posTermMachineServiceImpl")
 public class PosTermMachineServiceImpl  implements TermMachineService {
 
+    @Resource(name = "imsTermMachineService")
+    private ImsTermMachineService imsTermMachineService;
+
     @Override
     public List<TermMachineVo> queryTermMachine(PlatformType platformType) throws Exception{
-        return null;
+        List<ImsTermMachine> list =  imsTermMachineService.selectByExample();
+        List<TermMachineVo> termMachineVoList = new ArrayList<>();
+        for (ImsTermMachine imsTermMachine : list) {
+            TermMachineVo newvo = new TermMachineVo();
+            newvo.setId(imsTermMachine.getMachineId());
+            newvo.setMechineName(imsTermMachine.getModel()+"|"+imsTermMachine.getRemark());
+            termMachineVoList.add(newvo);
+        }
+        return termMachineVoList;
     }
 
     @Override
