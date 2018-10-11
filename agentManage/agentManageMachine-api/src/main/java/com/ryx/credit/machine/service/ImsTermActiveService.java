c2dc025a73058c73ad16cb8b2d181b7a4ea2478e
liudh <liudonghui@ruiyinxin.com>
2018-10-11 10:38:52 +0800

    service文件初始化

--- agentManage/agentManageMachine-api/src/main/java/com/ryx/credit/machine/service/ImsTermActiveService.java
+++ agentManage/agentManageMachine-api/src/main/java/com/ryx/credit/machine/service/ImsTermActiveService.java
@@ -1,7 +1,12 @@
 package com.ryx.credit.machine.service;
 
+import com.ryx.credit.machine.entity.ImsTermActive;
+
 /**
  * Created by RYX on 2018/10/11.
  */
 public interface ImsTermActiveService {
+
+    ImsTermActive selectByPrimaryKey(String posSn);
+
 }
--- agentManage/agentManageMachine-dubbo/src/main/java/com/ryx/credit/machine/service/impl/ImsTermActiveServiceImpl.java
+++ agentManage/agentManageMachine-dubbo/src/main/java/com/ryx/credit/machine/service/impl/ImsTermActiveServiceImpl.java
@@ -1,6 +1,9 @@
 package com.ryx.credit.machine.service.impl;
 
+import com.ryx.credit.machine.dao.ImsTermActiveMapper;
+import com.ryx.credit.machine.entity.ImsTermActive;
 import com.ryx.credit.machine.service.ImsTermActiveService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /***
@@ -12,5 +15,13 @@ import org.springframework.stereotype.Service;
  **/
 @Service("imsTermActiveService")
 public class ImsTermActiveServiceImpl implements ImsTermActiveService {
+
+    @Autowired
+    private ImsTermActiveMapper imsTermActiveMapper;
+
+    @Override
+    public ImsTermActive selectByPrimaryKey(String posSn){
+       return imsTermActiveMapper.selectByPrimaryKey(posSn);
+    }
 
 }
