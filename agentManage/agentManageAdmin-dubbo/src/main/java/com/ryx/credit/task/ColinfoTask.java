6e8eec01cf92b4c6e9f0fea373f3bc868e1257fd
liudh <liudonghui@ruiyinxin.com>
2018-10-18 15:52:01 +0800

    日志修改

--- agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/task/ColinfoTask.java
+++ agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/task/ColinfoTask.java
@@ -224,9 +224,9 @@ public class ColinfoTask {
                                 importAgent.setBatchcode(DateUtil.format(new Date()));
                                 importAgent.setcUser(aColinfoPayment.getcUser());
                                 if (1 != aimportService.insertAgentImportData(importAgent)) {
-                                    log.info("synColinfoToQueryPayment代理商账户修改同步业务平台");
+                                    log.info("synColinfoToQueryPayment代理商账户修改同步业务平台失败");
                                 } else {
-                                    log.info("synColinfoToQueryPayment代理商账户修改同步业务平台失败!");
+                                    log.info("synColinfoToQueryPayment代理商账户修改同步业务平台!");
                                 }
                             } catch (Exception e) {
                                 e.printStackTrace();
