2f77575337017031aa4a01c2a5244875988f8ec2
liuqianyuan-b <liuqianyuan-bj@ruiyinxin.com>
2018-10-29 17:01:30 +0800

    POS奖励impl

--- agentManage/agentManageProfit-dubbo/src/main/java/com/ryx/credit/profit/service/impl/PosRewardSDetailServiceImpl.java
+++ agentManage/agentManageProfit-dubbo/src/main/java/com/ryx/credit/profit/service/impl/PosRewardSDetailServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ryx.credit.profit.service.impl;
 
 import com.ryx.credit.profit.service.PosRewardSDetailService;
+import org.springframework.stereotype.Service;
 
+@Service("posRewardSDetailService")
 public class PosRewardSDetailServiceImpl implements PosRewardSDetailService {
 }
