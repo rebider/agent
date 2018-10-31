311e1dec1a1537d152e2574b09f03db4357613b4
cx <chenxiaoxiang@ruiyanxinde.com>
2018-10-31 11:39:43 +0800

    打款分条添加

--- agentManage/agentManage-common/src/main/java/com/ryx/credit/common/enumc/PamentIdType.java
+++ agentManage/agentManage-common/src/main/java/com/ryx/credit/common/enumc/PamentIdType.java
@@ -2,12 +2,14 @@ package com.ryx.credit.common.enumc;
 
 /**
  * Created by RYX on 2018/7/23.
- * 付款明细表里的ID源类型
+ * 付款明细表里的ID源类型PAMENTIDTYPE
  */
 public enum  PamentIdType {
 
    ORDER_FKD("ORDER_FKD","订单付款单"),
    ORDER_BCJ("ORDER_BCJ","补差价产生欠款打款"),
+   ORDER_OLD("ORDER_OLD","老数据导入"),
+   ORDER_PDT("ORDER_PDT","付款明细拆分"),
    ORDER_BZJ("ORDER_BZJ","订单保证金分期,服务费等分期类型");
     public String code;
 
