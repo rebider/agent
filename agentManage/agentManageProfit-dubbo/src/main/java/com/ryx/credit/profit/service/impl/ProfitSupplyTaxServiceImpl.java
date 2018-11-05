1777bd520a0c309e0d770898fee9fe3a3b78a52c
liuqianyuan-b <liuqianyuan-bj@ruiyinxin.com>
2018-11-05 18:29:46 +0800

    税点调整服务配置

--- agentManage/agentManageProfit-dubbo/src/main/java/com/ryx/credit/profit/service/impl/ProfitSupplyTaxServiceImpl.java
+++ agentManage/agentManageProfit-dubbo/src/main/java/com/ryx/credit/profit/service/impl/ProfitSupplyTaxServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ryx.credit.profit.service.impl;
 
 import com.ryx.credit.profit.service.ProfitSupplyTaxService;
+import org.springframework.stereotype.Service;
 
+@Service("profitSupplyTaxService")
 public class ProfitSupplyTaxServiceImpl implements ProfitSupplyTaxService {
 }
