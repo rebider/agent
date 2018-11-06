2d43f5c38f8d43543aa4e55e523e010cb85591dc
liuqianyuan-b <liuqianyuan-bj@ruiyinxin.com>
2018-11-06 15:23:22 +0800

    税点信息查询开发

--- agentManage/agentManageProfit-dubbo/src/main/java/com/ryx/credit/profit/dao/ProfitSupplyTaxMapper.java
+++ agentManage/agentManageProfit-dubbo/src/main/java/com/ryx/credit/profit/dao/ProfitSupplyTaxMapper.java
@@ -3,6 +3,8 @@ package com.ryx.credit.profit.dao;
 import com.ryx.credit.profit.pojo.ProfitSupplyTax;
 import com.ryx.credit.profit.pojo.ProfitSupplyTaxExample;
 import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Param;
 
 public interface ProfitSupplyTaxMapper {
@@ -19,4 +21,8 @@ public interface ProfitSupplyTaxMapper {
     int updateByExampleSelective(@Param("record") ProfitSupplyTax record, @Param("example") ProfitSupplyTaxExample example);
 
     int updateByExample(@Param("record") ProfitSupplyTax record, @Param("example") ProfitSupplyTaxExample example);
+
+    long getProfitSupplyTaxCount(Map<String, Object> param);
+
+    List<Map<String, Object>> getProfitSupplyTaxList(Map<String, Object> param);
 }
