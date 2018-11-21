d8fec29b55ff770d13f23c8a062a12993931638c
liudh <liudonghui@ruiyinxin.com>
2018-11-21 18:19:40 +0800

    订单导出bug修改

--- agentManage/agentManageAdmin-api/src/main/java/com/ryx/credit/pojo/admin/vo/OrderoutVo.java
+++ agentManage/agentManageAdmin-api/src/main/java/com/ryx/credit/pojo/admin/vo/OrderoutVo.java
@@ -22,6 +22,7 @@ public class OrderoutVo implements Serializable{
     private String downPaymentUser;//付款人
     private String comName;//收款地方
     private BigDecimal actualReceipt;//付款金额
+    private BigDecimal xxAmount;//付款金额
     private Date actualReceiptDate;//收款日期
     private String actualTime;
     private String oinuretime;
@@ -220,5 +221,13 @@ public class OrderoutVo implements Serializable{
 
     public void setAgDocPpro(String agDocPpro) {
         this.agDocPpro = agDocPpro;
+    }
+
+    public BigDecimal getXxAmount() {
+        return xxAmount;
+    }
+
+    public void setXxAmount(BigDecimal xxAmount) {
+        this.xxAmount = xxAmount;
     }
 }
