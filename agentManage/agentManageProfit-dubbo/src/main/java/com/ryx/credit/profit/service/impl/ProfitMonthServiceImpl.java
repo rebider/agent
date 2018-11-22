01f39316a7680d7ef8465b3c5cf2c3ab0408a889
zhaodawei <zhaodawei>
2018-11-22 17:14:10 +0800

    完善分润计算

--- agentManage/agentManageProfit-dubbo/src/main/java/com/ryx/credit/profit/service/impl/ProfitMonthServiceImpl.java
+++ agentManage/agentManageProfit-dubbo/src/main/java/com/ryx/credit/profit/service/impl/ProfitMonthServiceImpl.java
@@ -616,9 +616,10 @@ public class ProfitMonthServiceImpl implements ProfitMonthService {
         // mpos退单补款
         sumAmt = sumAmt.add(profitDetailMonthTemp.getMposTdSupplyAmt());
         // 其他补款
+        profitDetailMonthTemp.setOtherSupplyAmt(profitComputerService.new_total_supply(profitDetailMonthTemp.getAgentId(), profitDetailMonthTemp.getParentAgentId(), null));
         sumAmt = sumAmt.add(profitDetailMonthTemp.getOtherSupplyAmt());
         // 考核奖励
-        sumAmt = sumAmt.add(profitDetailMonthTemp.getPosRewardAmt());
+        sumAmt = sumAmt.add(profitDetailMonthTemp.getPosRewardAmt() == null?BigDecimal.ZERO:profitDetailMonthTemp.getPosRewardAmt());
                 //.subtract(profitDetailMonthTemp.getPosRewardDeductionAmt())
         //退单扣款-
         if (!profitDetailMonthTemp.getAgentId().startsWith("6000")) {
