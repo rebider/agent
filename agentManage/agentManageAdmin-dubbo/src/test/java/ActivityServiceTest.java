a3a8e913a71b0921a1afa250503c4cddecae9379
cx <chenxiaoxiang@ruiyanxinde.com>
2018-12-24 14:19:51 +0800

    机具线下打款月度统计表

---
+++ agentManage/agentManageAdmin-dubbo/src/test/java/ActivityServiceTest.java
@@ -0,0 +1,42 @@
+import com.ryx.credit.common.util.DateUtils;
+import com.ryx.credit.common.util.FastMap;
+import com.ryx.credit.service.ActivityService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+
+/**
+ * 作者：cx
+ * 时间：2018/12/21
+ * 描述：
+ */
+public class ActivityServiceTest extends BaseSpringTest {
+
+    @Autowired
+    private ActivityService activityService;
+
+    @Test
+    public void testInstance(){
+        activityService.createDeloyFlow(null,"process_agent_relation",null,null, FastMap.fastMap("part","south"));
+    }
+
+
+    @Test
+    public void testComplet(){
+
+        activityService.completeTaskInNer("1700008",FastMap.fastMap("rs","pass")
+        .putKeyV("approvalOpinion","approvalOpinion")
+                .putKeyV("approvalPerson","1")
+                .putKeyV("createTime", DateUtils.beforeDate(new Date())));
+        activityService.completeTaskInNer("1697520",FastMap.fastMap("rs","reject")
+                .putKeyV("approvalOpinion","approvalOpinion")
+                .putKeyV("approvalPerson","1")
+                .putKeyV("createTime", DateUtils.beforeDate(new Date())));
+    }
+
+
+
+
+
+}
