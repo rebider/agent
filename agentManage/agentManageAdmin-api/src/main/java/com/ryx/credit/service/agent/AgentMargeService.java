d9c7b788c766f765f0d9f3f1970e928ea0441bc3
liudh <liudonghui@ruiyinxin.com>
2019-01-07 16:30:24 +0800

    代理商合并

---
+++ agentManage/agentManageAdmin-api/src/main/java/com/ryx/credit/service/agent/AgentMargeService.java
@@ -0,0 +1,7 @@
+package com.ryx.credit.service.agent;
+
+/**
+ * Created by RYX on 2019/1/7.
+ */
+public interface AgentMargeService {
+}
---
+++ agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/service/impl/agent/AgentMargeServiceImpl.java
@@ -0,0 +1,16 @@
+package com.ryx.credit.service.impl.agent;
+
+import com.ryx.credit.service.agent.AgentMargeService;
+import org.springframework.stereotype.Service;
+
+/***
+ * 代理商合并
+ * @Author liudh
+ * @Description //TODO 
+ * @Date 2019/1/7 16:24
+ * @Param
+ * @return
+ **/
+@Service("agentMargeService")
+public class AgentMargeServiceImpl implements AgentMargeService {
+}
