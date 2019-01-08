0a3d2d7810cdb9ed7da0f0fc24a8ce03fb38baf6
liudh <liudonghui@ruiyinxin.com>
2019-01-08 10:41:49 +0800

    代理商合并工作流监听

--- agentManage/agentManageAdmin-api/src/main/java/com/ryx/credit/service/agent/AgentMergeService.java
+++ agentManage/agentManageAdmin-api/src/main/java/com/ryx/credit/service/agent/AgentMergeService.java
@@ -1,7 +1,13 @@
 package com.ryx.credit.service.agent;
 
+import com.ryx.credit.common.result.AgentResult;
+
+import java.math.BigDecimal;
+
 /**
  * Created by RYX on 2019/1/7.
  */
 public interface AgentMergeService {
+
+    AgentResult compressAgentMergeActivity(String proIns, BigDecimal agStatus)throws Exception;
 }
--- agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/service/impl/agent/AgentMergeServiceImpl.java
+++ agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/service/impl/agent/AgentMergeServiceImpl.java
@@ -1,7 +1,13 @@
 package com.ryx.credit.service.impl.agent;
 
+import com.ryx.credit.common.result.AgentResult;
 import com.ryx.credit.service.agent.AgentMergeService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
 
 /***
  * 代理商合并
@@ -13,4 +19,19 @@ import org.springframework.stereotype.Service;
  **/
 @Service("agentMergeService")
 public class AgentMergeServiceImpl implements AgentMergeService {
+
+
+    /**
+     * 审批结果监听
+     * @param proIns
+     * @param agStatus
+     * @return
+     * @throws Exception
+     */
+    @Transactional(rollbackFor = Exception.class, isolation = Isolation.DEFAULT, propagation = Propagation.REQUIRED)
+    @Override
+    public AgentResult compressAgentMergeActivity(String proIns, BigDecimal agStatus)throws Exception{
+
+        return AgentResult.ok();
+    }
 }
