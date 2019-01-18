98716191cef54137b8e7e4e61c399c19f81a1d3e
liudh <liudonghui@ruiyinxin.com>
2019-01-18 14:02:27 +0800

    代理商合并数据权限

--- agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/service/impl/agent/AgentMergeServiceImpl.java
+++ agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/service/impl/agent/AgentMergeServiceImpl.java
@@ -1187,16 +1187,15 @@ public class AgentMergeServiceImpl implements AgentMergeService {
      * @return
      */
     public AgentResult mPos_updateAgName(String agentName, List<String> mPosOrgList, AgentColinfo agentColinfo) {
-        HashMap<String,String> map = new HashMap<String,String>();
-
+        HashMap<String,Object> map = new HashMap<>();
         map.put("companyname", agentName);//代理商名称
-        map.put("batchIds", mPosOrgList.toString());//AG码
-        map.put("colinfoMessage", JsonUtil.objectToJson(agentColinfo));//收款账户
+        map.put("batchIds", mPosOrgList);//AG码
+        map.put("colinfoMessage", agentColinfo);//收款账户
         String params = JsonUtil.objectToJson(map);
-        logger.info("======mPos_updateAgName:{}",params);
+        logger.info("代理商合并通知手刷请求:{}",params);
         String res = HttpClientUtil.doPostJson
                 (AppConfig.getProperty("busiPlat.upAgName"),params);
-        logger.info("======mPos_updateAgName结果:{}",res);
+        logger.info("代理商合并通知手刷结果:{}",res);
         JSONObject resObj = JSONObject.parseObject(res);
         if(!resObj.get("respCode").equals("000000")){
             logger.error("请求失败！");
@@ -1289,6 +1288,14 @@ public class AgentMergeServiceImpl implements AgentMergeService {
         }
         if (StringUtils.isNotBlank(agentMergeBusInfo.getSubAgentId())) {
             reqMap.put("subAgentId", agentMergeBusInfo.getSubAgentId());
+        }
+        if(StringUtils.isBlank(dataRole)){
+            List<Map<String, Object>> orgCodeRes = iUserService.orgCode(userId);
+            if(orgCodeRes==null && orgCodeRes.size()!=1){
+                return null;
+            }
+            Map<String, Object> stringObjectMap = orgCodeRes.get(0);
+            reqMap.put("orgId",String.valueOf(stringObjectMap.get("ORGID")));
         }
         List<Map<String,Object>> agentMergeList = agentMergeBusInfoMapper.selectMergeBusinfoList(reqMap, page);
         PageInfo pageInfo = new PageInfo();
