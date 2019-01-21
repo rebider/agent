0baf0d256966e54e6450926c0949ad772dba05ae
chenliang <923705168@qq.com>
2019-01-21 09:03:51 +0800

    代理商合并后的代替扣款接口

---
+++ agentManage/agentManageProfit-api/src/main/java/com/ryx/credit/profit/service/IProfitMergeDeductionService.java
@@ -0,0 +1,18 @@
+package com.ryx.credit.profit.service;
+
+import com.ryx.credit.profit.pojo.ProfitDeduction;
+
+import java.util.Map;
+
+public interface IProfitMergeDeductionService {
+
+    /***
+     * @Description: 新增扣款信息
+     * @Param: 扣款对象
+     * @Author: chenliang
+     * @Date: 2019/1/17
+     */
+    int insert(ProfitDeduction deduction);
+
+    Map ProfitMergeDeduction(Map<String, String> params);
+}
