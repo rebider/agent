f04e14a19e113839d95424dfc16dc8916fbf67fd
liudh <liudonghui@ruiyinxin.com>
2019-01-28 09:38:44 +0800

    枚举

---
+++ agentManage/agentManage-common/src/main/java/com/ryx/credit/common/enumc/PlatformStatus.java
@@ -0,0 +1,49 @@
+package com.ryx.credit.common.enumc;
+
+import java.math.BigDecimal;
+
+
+/**
+ * 通知业务平台状态
+ * Created by liudh on 2018/9/58.
+ */
+public enum PlatformStatus {
+
+    SUCCESS(new BigDecimal("0"),"失败"),
+    FAIL(new BigDecimal("1"),"成功");
+
+
+    public BigDecimal key;
+    public String msg;
+
+    PlatformStatus(BigDecimal k, String s){
+        key = k;
+        msg = s;
+    }
+
+    /**
+     * 取得枚举对象值
+     * @return 枚举对象值
+     */
+    public BigDecimal getValue() {
+        return this.key;
+    }
+    /**
+     * 取得缓存内容
+     * @return 缓存内容
+     */
+    public String getContent() {
+        return this.msg;
+    }
+
+
+    public static String getContentByValue(BigDecimal value){
+        PlatformStatus[] busType = PlatformStatus.values();
+        for(PlatformStatus bt : busType){
+            if(bt.key.compareTo(value)==0){
+                return bt.msg;
+            }
+        }
+        return "";
+    }
+}
