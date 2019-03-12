db1cd6e4f78c7084d2e2896c874840b098e545ce
lhl <lihaili-bj@ruiyinxin.com>
2019-03-12 17:16:41 +0800

    解析订单生成数据

--- agentManage/agentManageAdmin-api/src/main/java/com/ryx/credit/pojo/admin/vo/OrderImportBaseInfo.java
+++ agentManage/agentManageAdmin-api/src/main/java/com/ryx/credit/pojo/admin/vo/OrderImportBaseInfo.java
@@ -14,12 +14,14 @@ import java.util.List;
  */
 public class OrderImportBaseInfo implements Serializable {
 
-    public static  List<String> colum  = Arrays.asList("order_id",
+    public static  List<String> colum  = Arrays.asList(
+            "order_id",
             "order_orgid",
             "order_platform",
-            "order_agname","order_date","order_amt","order_have_amt",
+            "order_agname",
+            "order_date","order_amt","order_have_amt",
             "order_paymethod","order_shoufu_amt","order_fenqi_count","order_fenqi_date","order_colcomp","order_pay_user","order_is_fp",
-            "order_dk_type","order_dk_amt","order_hkr","order_hk_date","order_remark");
+            "order_dk_type","order_dk_amt","order_hkr","order_hk_date","order_remark","ox_order");
 
     public void loadInfoFromJsonArray(JSONArray data,String importId){
         if(data!=null){
@@ -42,6 +44,7 @@ public class OrderImportBaseInfo implements Serializable {
             this.setOrder_hkr(data.getString(colum.indexOf("order_hkr")));
             this.setOrder_hk_date(data.getString(colum.indexOf("order_hk_date")));
             this.setOrder_remark(data.getString(colum.indexOf("order_remark")));
+            this.setOx_order(data.getString(colum.indexOf("ox_order")));
         }
         this.importId=importId;
     }
@@ -69,6 +72,15 @@ public class OrderImportBaseInfo implements Serializable {
     public  String order_remark ;
     public  List<OrderImportGoodsInfo> orderImportGoodsInfos;
 
+    public String ox_order;
+
+    public String getOx_order() {
+        return ox_order;
+    }
+
+    public void setOx_order(String ox_order) {
+        this.ox_order = ox_order;
+    }
 
     public String getImportId() {
         return importId;
---
+++ agentManage/agentManageAdmin-api/src/main/java/com/ryx/credit/service/order/NewOrderImportService.java
@@ -0,0 +1,94 @@
+package com.ryx.credit.service.order;
+
+import com.ryx.credit.common.exception.MessageException;
+import com.ryx.credit.common.result.AgentResult;
+import com.ryx.credit.common.util.ResultVO;
+import com.ryx.credit.pojo.admin.agent.ImportAgent;
+import com.ryx.credit.pojo.admin.order.OOrder;
+import com.ryx.credit.pojo.admin.order.OPayment;
+import com.ryx.credit.pojo.admin.vo.OrderImportBaseInfo;
+import java.util.List;
+
+/**
+ * Created by lhl on 2019/3/5.
+ */
+public interface NewOrderImportService {
+
+    /**
+     * 解析订单数据
+     * @param user
+     * @return
+     */
+    public ResultVO newPareseOrderEnter(String user);
+
+
+    /**
+     * 解析退货订单数据
+     * @param user
+     * @return
+     */
+    public ResultVO newPareseReturnOrderEnter(String user);
+
+
+    /**
+     * 将Excel
+     * @param data
+     * @param dataType
+     * @param user
+     * @param batch
+     * @return
+     * @throws Exception
+     */
+    public List<String> newAddOrderInfoList(List<List<Object>> data, String dataType, String user, String batch)throws Exception;
+
+
+    /**
+     * 处理单个订单
+     * @return
+     * @throws Exception
+     */
+    public AgentResult newPareseOrder(ImportAgent importAgent, String User)throws MessageException;
+
+
+    /**
+     * 解析生成订单及付款单信息
+     * @param orderImportBaseInfo
+     * @param User
+     * @return
+     * @throws Exception
+     */
+    public AgentResult newPareseOrderImportBaseInfo(OrderImportBaseInfo orderImportBaseInfo, String User)throws MessageException;
+
+
+    /**
+     * 解析生成订单子订单，子订单活动信息
+     * @param orderImportBaseInfo
+     * @param User
+     * @return
+     * @throws Exception
+     */
+    public AgentResult newPareseOrderImportSubOrderInfo(OrderImportBaseInfo orderImportBaseInfo, OOrder order, OPayment oPayment, String User)throws MessageException;
+
+
+    /**
+     * 处理订单物流
+     * 处理订单物流
+     * @return
+     * @throws Exception
+     */
+    public AgentResult newPareseOrderLogic(String value)throws MessageException;
+
+
+    /**
+     * 处理退货单
+     * @return
+     * @throws Exception
+     */
+    public AgentResult newPareseReturn(ImportAgent importAgent, String User)throws MessageException;
+
+
+    public AgentResult newDeleteFailImportAgentReturn(ImportAgent importAgent, String user)throws Exception;
+
+
+    public AgentResult newDeleteFailImportAgentOrder(ImportAgent importAgent, String user)throws Exception;
+}
