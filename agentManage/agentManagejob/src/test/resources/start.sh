0b5a9c5598eef3acdefbc06e7725e7ceead931f6
cx <chenxiaoxiang@ruiyanxinde.com>
2019-04-04 13:24:35 +0800

    删除

--- agentManage/agentManagejob/src/test/resources/start.sh
+++
@@ -1,2 +0,0 @@
-java -Xms800m -Xmx800m -XX:PermSize=256m -XX:MaxPermSize=512m -XX:MaxNewSize=512m -jar agentManagejob.jar >/dev/null 2>&1 &
-tail -f /home/ryx/agentManager-job-dubbo/agentManager-job-debug.log
