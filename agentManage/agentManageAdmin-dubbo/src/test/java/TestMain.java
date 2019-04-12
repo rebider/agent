b26a180d07fb5fd86a3cd2f0ecbe39632ed5ce0f
lirongrong <lirongrong@ruiyanxinde.com>
2019-04-12 17:36:03 +0800

    截取SN的位数

--- agentManage/agentManageAdmin-dubbo/src/test/java/TestMain.java
+++ agentManage/agentManageAdmin-dubbo/src/test/java/TestMain.java
@@ -1,4 +1,7 @@
+import com.ryx.credit.common.util.Conver10ToConver33Utils;
 
+import java.util.Map;
+import java.util.regex.Pattern;
 
 /**
  * Created by cx on 2018/6/12.
@@ -6,8 +9,20 @@
 public class TestMain {
 
     public static void main(String[] args) {
-        System.out.println("LDM保定M晟川商贸有限公司(风扇)(asdf)");
-        System.out.println("LDM保定M晟川商贸有限公司".replaceAll("[A-Z]|\\(*\\)",""));
-        System.out.println(System.getProperty("java.io.tmpdir"));
+        String a="1003A30";
+        String b="1003S30";
+        Map digit = Conver10ToConver33Utils.getDigit(a, b);
+        String start=(String)digit.get("start");
+        String end=(String)digit.get("end");
+        Integer length=(Integer)digit.get("length");
+        Integer num=(Integer)digit.get("num");
+        Pattern p = Pattern.compile("[a-zA-z]");
+        if(p.matcher(start).find() || p.matcher(end).find())
+        {
+            System.out.println("含有英文字符"+"----起始位数:"+num+"---结束位数:"+length);
+        }else{
+            System.out.println("不含英文字符");
+        }
+
     }
 }
