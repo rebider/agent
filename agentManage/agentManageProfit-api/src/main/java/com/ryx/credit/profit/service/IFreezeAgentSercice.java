a4bee01cbf9ea85e0005276e7e4a84cf5a403477
chenliang <chenliang-jn@ruiyinxin.com>
2019-04-24 15:59:42 +0800

    冻结申请

--- agentManage/agentManageProfit-api/src/main/java/com/ryx/credit/profit/service/IFreezeAgentSercice.java
+++ agentManage/agentManageProfit-api/src/main/java/com/ryx/credit/profit/service/IFreezeAgentSercice.java
@@ -2,12 +2,15 @@ package com.ryx.credit.profit.service;
 
 import com.ryx.credit.common.util.Page;
 import com.ryx.credit.common.util.PageInfo;
+import com.ryx.credit.profit.pojo.FreezeOperationRecord;
 import com.ryx.credit.profit.pojo.FreezeAgent;
 
+import java.util.List;
 import java.util.Map;
 
 public interface IFreezeAgentSercice {
     PageInfo getselectFreezeDate(Map<String, Object> param, PageInfo pageInfo);
+    public void operationFreezeDate(List<FreezeOperationRecord> freezeOperationRecords, String user);
 
     PageInfo getFreezeData(FreezeAgent freezeAgent, String isQuerySubordinate, Page page, String orgId);
 }
--- agentManage/agentManageProfit-dubbo/src/main/java/com/ryx/credit/profit/service/impl/FreezeAgentSerciceImpl.java
+++ agentManage/agentManageProfit-dubbo/src/main/java/com/ryx/credit/profit/service/impl/FreezeAgentSerciceImpl.java
@@ -1,20 +1,33 @@
 package com.ryx.credit.profit.service.impl;
 
 import com.alibaba.fastjson.JSONObject;
+import com.ryx.credit.common.enumc.TabId;
 import com.ryx.credit.common.util.Page;
 import com.ryx.credit.common.util.PageInfo;
 import com.ryx.credit.commons.utils.StringUtils;
 import com.ryx.credit.profit.dao.FreezeAgentMapper;
+import com.ryx.credit.profit.dao.FreezeOperationRecordMapper;
 import com.ryx.credit.profit.pojo.FreezeAgent;
 import com.ryx.credit.profit.pojo.FreezeAgentExample;
+import com.ryx.credit.profit.pojo.FreezeOperationRecord;
+import com.ryx.credit.profit.pojo.ProfitDetailMonth;
 import com.ryx.credit.profit.service.IFreezeAgentSercice;
+import com.ryx.credit.profit.service.ProfitDetailMonthService;
+import com.ryx.credit.service.dict.IdService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * chenliang
@@ -25,15 +38,24 @@ public class FreezeAgentSerciceImpl implements IFreezeAgentSercice {
     Logger logger = LoggerFactory.getLogger(FreezeAgentSerciceImpl.class);
     @Autowired
     FreezeAgentMapper freezeAgentMapper;
+    @Autowired
+    ProfitDetailMonthService profitDetailMonthService;
+    @Autowired
+    private IdService idService;
+    @Autowired
+    FreezeOperationRecordMapper freezeOperationRecordMapper;
+
 
     @Override
     public PageInfo getselectFreezeDate(Map<String, Object> param, PageInfo pageInfo){
        Integer count = 0;
         List<FreezeAgent> listAll;
         if ("true".equals(param.get("isQuerySubordinate"))){
+            logger.info("不含下级查询");
             listAll = freezeAgentMapper.selectAllNotFreezeLower(param);
             count = freezeAgentMapper.selectAllNotFreezeLowerCount(param);
         }else{
+            logger.info("下级查询");
             listAll = freezeAgentMapper.selectAllNotFreeze(param);
             count = freezeAgentMapper.selectAllNotFreezeCount(param);
         }
@@ -44,6 +66,108 @@ public class FreezeAgentSerciceImpl implements IFreezeAgentSercice {
         return pageInfo;
 
   }
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.DEFAULT, rollbackFor = Exception.class)
+    public void operationFreezeDate(List<FreezeOperationRecord> freezeOperationRecords,String user){
+       if(freezeOperationRecords!=null){
+           for (FreezeOperationRecord freezeOperationRecord:freezeOperationRecords) {
+               String uuid = UUID.randomUUID().toString().replaceAll("-", "");
+               //更新月份润状态
+               ProfitDetailMonth profitDetailMonth1;
+               if("00".equals(freezeOperationRecord.getFreezeType())){
+                   try{
+                       ProfitDetailMonth profitDetailMonth = new ProfitDetailMonth();
+                       profitDetailMonth.setAgentId(freezeOperationRecord.getAgentId());
+                       profitDetailMonth.setParentAgentId(freezeOperationRecord.getParentAgentId());
+                       profitDetailMonth.setParentAgentId(LocalDate.now().plusMonths(-1).toString().substring(0, 7).replaceAll("-", ""));
+                       profitDetailMonth1 =  profitDetailMonthService.selectByPIdAndMonth(profitDetailMonth);
+                       profitDetailMonth1.setStatus("1");
+                       profitDetailMonthService.updateByPrimaryKeySelective(profitDetailMonth1);
+                       freezeOperationRecord.setFreezeAmt(profitDetailMonth1.getBasicsProfitAmt());
+                   }catch (Exception e){
+                       e.printStackTrace();
+                       logger.info("更新月份润冻结状态失败");
+                       throw new RuntimeException("更新月份润冻结状态失败");
+                   }
+
+
+               }
+
+               //插入操作表新的数据
+               try{
+
+                   Date date = new Date();
+                   SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                   freezeOperationRecord.setOperationTime(sdf.format(date));
+                   freezeOperationRecord.setFreezeBatch(uuid);
+                   freezeOperationRecord.setId(idService.genId(TabId.P_FREEZE_OPERATION_RECORD));
+                   freezeOperationRecord.setStatus("0");
+                   freezeOperationRecordMapper.insertSelective(freezeOperationRecord);
+               }catch (Exception e){
+                   e.printStackTrace();
+                   logger.info("插入操作明细失败");
+                   throw new RuntimeException("插入操作明细失败");
+               }
+
+               //更新或者插入冻结解冻表
+               try{
+                   FreezeAgentExample freezeAgentExample = new FreezeAgentExample();
+                   FreezeAgentExample.Criteria criteria = freezeAgentExample.createCriteria();
+                   FreezeAgent freezeAgent = new FreezeAgent();
+                   if(StringUtils.isNotBlank(freezeOperationRecord.getAgentId())){
+                       criteria.andAgentIdEqualTo(freezeOperationRecord.getAgentId());
+                       freezeAgent.setAgentId(freezeOperationRecord.getAgentId());
+                   }
+                   if(StringUtils.isNotBlank(freezeOperationRecord.getAgentName())){
+                       criteria.andAgentNameEqualTo(freezeOperationRecord.getAgentName());
+                       freezeAgent.setAgentName(freezeOperationRecord.getAgentName());
+                   }
+                   if(StringUtils.isNotBlank(freezeOperationRecord.getParentAgentId())){
+                       criteria.andParentAgentIdEqualTo(freezeOperationRecord.getParentAgentId());
+                       freezeAgent.setParentAgentId(freezeOperationRecord.getParentAgentId());
+                   }
+                   if(StringUtils.isNotBlank(freezeOperationRecord.getParentAgentName())){
+                       criteria.andParentAgentNameEqualTo(freezeOperationRecord.getParentAgentName());
+                       freezeAgent.setParentAgentName(freezeOperationRecord.getParentAgentName());
+                   }
+                   if(StringUtils.isNotBlank(freezeOperationRecord.getFreezeType())){
+                       criteria.andFreezeTypeEqualTo(freezeOperationRecord.getFreezeType());
+                       freezeAgent.setFreezeType(freezeOperationRecord.getFreezeType());
+                   }
+                   List<FreezeAgent> freezeAgents=  freezeAgentMapper.selectByExample(freezeAgentExample);
+                   if(freezeAgents.size() ==1 ){
+                       if(freezeAgents.get(0).getStatus()=="0"){
+                           freezeAgents.get(0).setStatus("1");
+                           freezeAgents.get(0).setOperationBatch(uuid);
+                           freezeAgentMapper.updateByPrimaryKeySelective(freezeAgents.get(0));
+                       }
+
+                   }else if(freezeAgents.size()>1){
+                       logger.info("查询冻结解冻数据时失败");
+                       throw new RuntimeException("查询冻结解冻数据时失败");
+                   }else{
+                       freezeAgent.setId((idService.genId(TabId.P_FREEZE_AGENT)));
+                       freezeAgent.setOperationBatch(uuid);
+                       freezeAgentMapper.insertSelective(freezeAgent);
+                   }
+
+
+
+               }catch (Exception e){
+                   e.printStackTrace();
+
+               }
+           }
+       }else{
+           logger.info("未选择代理商" );
+           throw new RuntimeException("未选择代理商");
+       }
+
+    }
+
+
+
+
 
     @Override
     public PageInfo getFreezeData(FreezeAgent freezeAgent, String isQuerySubordinate, Page page, String orgId) {
