bd85741c8550e1e853550bde8545955deebd727b
liudh <liudonghui-bj@ruiyinxin.com>
2019-06-03 09:27:48 +0800

    活动权限配置优化

--- agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/dao/order/OActivityMapper.java
+++ agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/dao/order/OActivityMapper.java
@@ -26,4 +26,6 @@ public interface OActivityMapper {
     List<Map<String,Object>> productActivityOrderBuild(Map par);
 
     List<OActivity> planChoiseProComAndModel(Map par);
+
+    int updateByActCode(OActivity record);
 }
