b9f6002f2b137eaae792c45a4bc27fa05c38a7da
lirongrong <lirongrong@ruiyanxinde.com>
2019-07-09 17:31:39 +0800

    生成实体

--- agentManage/agentManageAdmin-api/src/main/java/com/ryx/credit/pojo/admin/agent/Agent.java
+++ agentManage/agentManageAdmin-api/src/main/java/com/ryx/credit/pojo/admin/agent/Agent.java
@@ -87,9 +87,29 @@ public class Agent extends AgentExtends implements Serializable {
 
     private BigDecimal freestatus;
 
-    private String busRiskEmail ;
+    private String busRiskEmail;
 
     private String busContactEmail;
+
+    private BigDecimal reportStatus;
+
+    private Date reportTime;
+
+    public BigDecimal getReportStatus() {
+        return reportStatus;
+    }
+
+    public void setReportStatus(BigDecimal reportStatus) {
+        this.reportStatus = reportStatus;
+    }
+
+    public Date getReportTime() {
+        return reportTime;
+    }
+
+    public void setReportTime(Date reportTime) {
+        this.reportTime = reportTime;
+    }
 
     public String getId() {
         return id;
@@ -370,4 +390,6 @@ public class Agent extends AgentExtends implements Serializable {
     public void setBusContactEmail(String busContactEmail) {
         this.busContactEmail = busContactEmail;
     }
+
+
 }
