993ee4e305670b17fc94b1dcbbfb3efad47ab443
liudh <liudonghui-bj@ruiyinxin.com>
2019-07-17 15:21:45 +0800

    优化

--- agentManage/agentManageInternetCard-dubbo/src/main/sql/internet_renew.sql
+++ agentManage/agentManageInternetCard-dubbo/src/main/sql/internet_renew.sql
@@ -4,7 +4,7 @@ select flow_id,renew_id,renew_detail_id,iccid_num,agent_id,agent_name,mer_id,
   mer_name,offset_amt,already_offset_amt,c_time,c_user,u_user,status,version,CLEAN_STATUS
   from o_internet_renew_offset
 where status =1 and offset_amt!=already_offset_amt
-and trunc(c_time) = trunc(sysdate-1)
+and c_time = trunc(to_char(sysdate,'YYYYMMDD')-1)
 
 
 <!-- 物联网卡续费已轧差金额清结算同步到代理商  每天0：30 勿删 -->
