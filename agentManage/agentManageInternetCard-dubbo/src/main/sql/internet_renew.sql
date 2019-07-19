bb0bc33b3f96d1f82c2f61b9d613db79115c8d29
liudh <liudonghui-bj@ruiyinxin.com>
2019-07-19 15:00:03 +0800

    bug修改

--- agentManage/agentManageInternetCard-dubbo/src/main/sql/internet_renew.sql
+++ agentManage/agentManageInternetCard-dubbo/src/main/sql/internet_renew.sql
@@ -9,7 +9,7 @@ and c_time = trunc(to_char(sysdate,'YYYYMMDD')-1)
 
 <!-- 物联网卡续费已轧差金额清结算同步到代理商  每天0：30 勿删 -->
 select
-flow_id,already_offset_amt,CLEAN_STATUS
+flow_id,already_offset_amt,CLEAN_STATUS,PROCESS_DATE,PROCESS_TIME
 from o_Internet_renew_offset
 where status=1 and CLEAN_STATUS!=0
 and PROCESS_DATE = trunc(to_char(sysdate,'YYYYMMDD')-1)
