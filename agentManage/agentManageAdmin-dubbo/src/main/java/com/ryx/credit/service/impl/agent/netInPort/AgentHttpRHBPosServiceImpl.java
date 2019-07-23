064fd50976ebbfa92fbcd4e6c0f1350901b5857e
liudh <liudonghui-bj@ruiyinxin.com>
2019-07-23 17:41:07 +0800

    瑞花宝入网

--- agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/service/impl/agent/netInPort/AgentHttpRHBPosServiceImpl.java
+++ agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/service/impl/agent/netInPort/AgentHttpRHBPosServiceImpl.java
@@ -141,21 +141,76 @@ public class AgentHttpRHBPosServiceImpl implements AgentNetInHttpService {
 
     /**
      * 升级组装参数
-     * @param data
+     * @param param
      * @return
      */
     @Override
-    public Map agencyLevelUpdateChangeData(Map data) {
-        Map data_res = new HashMap();
-        try {
+    public Map agencyLevelUpdateChangeData(Map param) {
+        Map<String, Object> resultMap = new HashMap<>();
+        AgentBusInfo agentBusInfo = (AgentBusInfo)param.get("agentBusInfo");
+        Agent agent = (Agent)param.get("agent");
+        PlatForm platForm = (PlatForm)param.get("platForm");
 
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            data_res.put("msg",e.getLocalizedMessage());
+        resultMap.put("application","AgencyPromotion");
+        resultMap.put("useOrgan",agentBusInfo.getBusUseOrgan()); //使用范围
+        resultMap.put("agCode",agent.getId());
+        resultMap.put("paymentAgency",agentBusInfo.getFinaceRemitOrgan());//出款机构
+        resultMap.put("orgId",agentBusInfo.getId());
+        resultMap.put("orgName",agent.getAgName());
+        if(agent.getAgRegArea()!=null){
+            List<String> regionList = agentNetInNotityService.getParent(agent.getAgRegArea());
+            if(regionList!=null){
+                if(regionList.size()==3){
+                    resultMap.put("province",regionList.get(0));
+                    resultMap.put("city",regionList.get(1));
+                    resultMap.put("cityArea",regionList.get(2));
+                }else if(regionList.size()==2){
+                    resultMap.put("province",regionList.get(0));
+                    resultMap.put("city",regionList.get(1));
+                }else if(regionList.size()==1){
+                    resultMap.put("province",regionList.get(0));
+                }
+            }
         }
-        return data_res;
+        resultMap.put("orgType",OrgType.zQ(agentBusInfo.getBusType())?OrgType.STR.getValue():OrgType.ORG.getValue());
+        AgentBusInfo agentParent = null;
+        if(StringUtils.isNotBlank(agentBusInfo.getBusParent())){
+            //取出上级业务
+            agentParent = agentBusInfoMapper.selectByPrimaryKey(agentBusInfo.getBusParent());
+        }
+        if(null!=agentParent){
+            resultMap.put("supDorgId",agentParent.getBusNum());
+        }
+        resultMap.put("alwaysProfit","00");//该机构是否参与实时分润
+        resultMap.put("brandName",platForm.getPlatformName());//平台名称
+        //收款账户新
+        AgentColinfo agentColinfo = agentColinfoService.selectByAgentIdAndBusId(agent.getId(), agentBusInfo.getId());
+        if(agentColinfo==null){
+            log.info("收款账户为空:{},{}",agent.getId(), agentBusInfo.getId());
+        }else{
+            agentColinfo = new AgentColinfo();
+        }
+        resultMap.put("agentId",agentBusInfo.getBusNum());//业务平台编号RHB
+
+        //00:对公账户，01：对私账户
+        resultMap.put("acctType",agentColinfo.getCloType().compareTo(BigDecimal.ONE)==0?"00":"01");
+        resultMap.put("credName",agent.getAgLegal());//法人姓名
+        resultMap.put("credNo",agent.getAgLegalCernum());//法人身份证
+        resultMap.put("credPhone",agent.getAgLegalMobile());//法人手机号
+        resultMap.put("dutyName",agent.getAgHead());//负责人
+        resultMap.put("dutyPhone",agent.getAgHeadMobile());//负责人手机号
+        resultMap.put("businesslicense",agent.getAgBusLic());//营业执照
+        resultMap.put("companyNature",agent.getAgNature());//公司性质
+        resultMap.put("bankCardName",agentColinfo.getCloRealname());//结算户名
+        resultMap.put("bankCard",agentColinfo.getCloBankAccount());//结算卡号
+        resultMap.put("bankCardCredNo",agentColinfo.getAgLegalCernum());//结算卡户主身份证
+        resultMap.put("openBank",agentColinfo.getCloBankCode());//收款开户总行 银行代码
+        resultMap.put("openBankChild",agentColinfo.getBranchLineNum());//收款开户支行 联号
+        resultMap.put("openBankChildName",agentColinfo.getCloBankBranch());//收款开户支行
+        resultMap.put("isBill",agentColinfo.getCloInvoice());//是否开具分润发票
+        resultMap.put("taxPoint",agentColinfo.getCloTaxPoint());//税点
+        resultMap.put("agencyMobileNo",agentBusInfo.getBusLoginNum());//代理商登录管理app手机号
+        return resultMap;
     }
 
 
@@ -175,9 +230,70 @@ public class AgentHttpRHBPosServiceImpl implements AgentNetInHttpService {
 
     @Override
     public Map<String, Object> packageParamUpdate(Map<String, Object> param) {
+        Map<String, Object> resultMap = new HashMap<>();
+        AgentBusInfo agentBusInfo = (AgentBusInfo)param.get("agentBusInfo");
+        Agent agent = (Agent)param.get("agent");
+        PlatForm platForm = (PlatForm)param.get("platForm");
 
+        resultMap.put("application","AgencyUpdateInfo");
+        resultMap.put("useOrgan",agentBusInfo.getBusUseOrgan()); //使用范围
+        resultMap.put("paymentAgency",agentBusInfo.getFinaceRemitOrgan());//出款机构
+        resultMap.put("orgId",agentBusInfo.getId());
+        resultMap.put("orgName",agent.getAgName());
+        if(agent.getAgRegArea()!=null){
+            List<String> regionList = agentNetInNotityService.getParent(agent.getAgRegArea());
+            if(regionList!=null){
+                if(regionList.size()==3){
+                    resultMap.put("province",regionList.get(0));
+                    resultMap.put("city",regionList.get(1));
+                    resultMap.put("cityArea",regionList.get(2));
+                }else if(regionList.size()==2){
+                    resultMap.put("province",regionList.get(0));
+                    resultMap.put("city",regionList.get(1));
+                }else if(regionList.size()==1){
+                    resultMap.put("province",regionList.get(0));
+                }
+            }
+        }
+        resultMap.put("orgType",OrgType.zQ(agentBusInfo.getBusType())?OrgType.STR.getValue():OrgType.ORG.getValue());
+        AgentBusInfo agentParent = null;
+        if(StringUtils.isNotBlank(agentBusInfo.getBusParent())){
+            //取出上级业务
+            agentParent = agentBusInfoMapper.selectByPrimaryKey(agentBusInfo.getBusParent());
+        }
+        if(null!=agentParent){
+            resultMap.put("supDorgId",agentParent.getBusNum());
+        }
+        resultMap.put("alwaysProfit","00");//该机构是否参与实时分润
+        resultMap.put("brandName",platForm.getPlatformName());//平台名称
+        //收款账户新
+        AgentColinfo agentColinfo = agentColinfoService.selectByAgentIdAndBusId(agent.getId(), agentBusInfo.getId());
+        if(agentColinfo==null){
+            log.info("收款账户为空:{},{}",agent.getId(), agentBusInfo.getId());
+        }else{
+            agentColinfo = new AgentColinfo();
+        }
+        resultMap.put("agentId",agentBusInfo.getBusNum());
 
-        return null;
+        //00:对公账户，01：对私账户
+        resultMap.put("acctType",agentColinfo.getCloType().compareTo(BigDecimal.ONE)==0?"00":"01");
+        resultMap.put("credName",agent.getAgLegal());//法人姓名
+        resultMap.put("credNo",agent.getAgLegalCernum());//法人身份证
+        resultMap.put("credPhone",agent.getAgLegalMobile());//法人手机号
+        resultMap.put("dutyName",agent.getAgHead());//负责人
+        resultMap.put("dutyPhone",agent.getAgHeadMobile());//负责人手机号
+        resultMap.put("businesslicense",agent.getAgBusLic());//营业执照
+        resultMap.put("companyNature",agent.getAgNature());//公司性质
+        resultMap.put("bankCardName",agentColinfo.getCloRealname());//结算户名
+        resultMap.put("bankCard",agentColinfo.getCloBankAccount());//结算卡号
+        resultMap.put("bankCardCredNo",agentColinfo.getAgLegalCernum());//结算卡户主身份证
+        resultMap.put("openBank",agentColinfo.getCloBankCode());//收款开户总行 银行代码
+        resultMap.put("openBankChild",agentColinfo.getBranchLineNum());//收款开户支行 联号
+        resultMap.put("openBankChildName",agentColinfo.getCloBankBranch());//收款开户支行
+        resultMap.put("isBill",agentColinfo.getCloInvoice());//是否开具分润发票
+        resultMap.put("taxPoint",agentColinfo.getCloTaxPoint());//税点
+        resultMap.put("agencyMobileNo",agentBusInfo.getBusLoginNum());//代理商登录管理app手机号
+        return resultMap;
     }
 
     @Override
