732baddb525e93e13ab4f3efa8e6546db892fdef
cx <chenxiaoxiang@ruiyanxinde.com>
2019-08-01 15:41:01 +0800

    实时分润借记费率参数调整

--- agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/service/impl/agent/netInPort/AgentHttpSsPosServiceImpl.java
+++ agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/service/impl/agent/netInPort/AgentHttpSsPosServiceImpl.java
@@ -114,6 +114,10 @@ public class AgentHttpSsPosServiceImpl implements AgentNetInHttpService  {
         resultMap.put("debitTop",agentBusInfo.getDebitCapping());
         resultMap.put("ckDebitRate",agentBusInfo.getDebitAppearRate());
         resultMap.put("lowDebitRate",agentBusInfo.getDebitRateLower());
+
+        resultMap.put("lowCreditRate",agentBusInfo.getCreditRateFloor());
+        resultMap.put("ceilingCreditRate",agentBusInfo.getCreditRateCeiling());
+
         resultMap.put("hasS0",agentBusInfo.getDredgeS0().equals(new BigDecimal(1))?"0":"1");
         resultMap.put("orgName",agent.getAgName());
         resultMap.put("useOrgan",agentBusInfo.getBusUseOrgan()); //使用范围
@@ -196,6 +200,10 @@ public class AgentHttpSsPosServiceImpl implements AgentNetInHttpService  {
             data.put("debitTop",paramMap.get("debitTop"));
             data.put("ckDebitRate",paramMap.get("ckDebitRate"));
             data.put("lowDebitRate",paramMap.get("lowDebitRate"));
+
+            data.put("lowCreditRate",paramMap.get("lowCreditRate"));
+            data.put("ceilingCreditRate",paramMap.get("ceilingCreditRate"));
+
             if(StringUtils.isNotBlank(String.valueOf(paramMap.get("orgId")))){
                 data.put("orgId",paramMap.get("orgId"));
             }
