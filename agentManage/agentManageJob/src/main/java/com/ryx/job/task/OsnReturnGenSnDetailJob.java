bec97c1dacd4fff9327528677c45eb4b461955eb
zhaoyade-bj <zhaoyade-bj@ruiyinxin.com>
2019-09-25 11:05:00 +0800

    up_z

--- agentManage/agentManageJob/src/main/java/com/ryx/job/task/OsnReturnGenSnDetailJob.java
+++ agentManage/agentManageJob/src/main/java/com/ryx/job/task/OsnReturnGenSnDetailJob.java
@@ -2,12 +2,11 @@ package com.ryx.job.task;
 
 import com.dangdang.ddframe.job.api.ShardingContext;
 import com.dangdang.ddframe.job.api.simple.SimpleJob;
-import com.ryx.credit.service.order.OsnOperateService;
+import com.ryx.credit.service.order.OsnOperateReturnService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.Resource;
 
 /**
  * 作者：cx
@@ -19,13 +18,13 @@ public class OsnReturnGenSnDetailJob implements SimpleJob {
 
     private static Logger logger = LoggerFactory.getLogger(OsnReturnGenSnDetailJob.class);
 
-    @Resource(name = "osnOperateReturnServiceImpl")
-    private OsnOperateService osnReturnOperateService;
+    @Autowired
+    private OsnOperateReturnService osnOperateReturnService;
 
     @Override
     public void execute(ShardingContext shardingContext) {
         logger.debug("退货发货sn明细生成任务开始执行");
-        osnReturnOperateService.genLogicDetailTask();
+        osnOperateReturnService.genLogicDetailTask();
         logger.debug("退货发货sn明细生成任务结束执行");
     }
 }
