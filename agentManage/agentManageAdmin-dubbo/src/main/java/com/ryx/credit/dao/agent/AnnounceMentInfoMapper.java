fd3bb329532377a923a2405aa7cbf3db2b49ec24
sunshengxi <sunshengxi-bj@ruiyinxin.com>
2019-10-14 17:33:53 +0800

    公告信息表，公告关联表dao

---
+++ agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/dao/agent/AnnounceMentInfoMapper.java
@@ -0,0 +1,37 @@
+package com.ryx.credit.dao.agent;
+
+import com.ryx.credit.common.util.Page;
+import com.ryx.credit.pojo.admin.agent.AnnounceMentInfo;
+import com.ryx.credit.pojo.admin.agent.AnnounceMentInfoExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
+public interface AnnounceMentInfoMapper {
+    long countByExample(AnnounceMentInfoExample example);
+
+    int deleteByExample(AnnounceMentInfoExample example);
+
+    int insert(AnnounceMentInfo record);
+
+    int insertSelective(AnnounceMentInfo record);
+
+    List<AnnounceMentInfo> selectByExampleWithBLOBs(AnnounceMentInfoExample example);
+
+    List<AnnounceMentInfo> selectByExample(AnnounceMentInfoExample example);
+
+    AnnounceMentInfo selectByPrimaryKey(String annId);
+
+    int updateByPrimaryKeySelective(AnnounceMentInfo record);
+
+    int updateByPrimaryKeyWithBLOBs(AnnounceMentInfo record);
+
+    int updateByPrimaryKey(AnnounceMentInfo record);
+
+    List<Map<String,Object>> selectAnnRead( @Param("map")Map<String, Object> map,@Param("page") Page page);
+
+    int selectCountAnnRead(@Param("map")Map<String,Object> map);
+
+    int updateStatByAnno(AnnounceMentInfo record);
+}
