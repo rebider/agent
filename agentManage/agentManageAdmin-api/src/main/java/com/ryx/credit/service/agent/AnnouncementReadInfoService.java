1c0b14d0344a8521cc4aa773281a299567e26eb2
sunshengxi <sunshengxi-bj@ruiyinxin.com>
2019-10-15 11:31:22 +0800

    公告查看记录

---
+++ agentManage/agentManageAdmin-api/src/main/java/com/ryx/credit/service/agent/AnnouncementReadInfoService.java
@@ -0,0 +1,14 @@
+package com.ryx.credit.service.agent;
+
+import com.ryx.credit.common.util.ResultVO;
+import com.ryx.credit.pojo.admin.agent.AnnouncementReadInfo;
+
+/**
+ * @program: agentManage
+ * @description:
+ * @author: ssx
+ * @create: 2019-10-15 10:32
+ **/
+public interface AnnouncementReadInfoService {
+    ResultVO saveResdInfo(AnnouncementReadInfo announcementReadInfo);
+}
