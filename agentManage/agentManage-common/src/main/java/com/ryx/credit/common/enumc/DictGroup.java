e2cc657daa290fdccb353749480686a9b97da9ab
lirongrong <lirongrong@ruiyanxinde.com>
2019-10-16 16:49:46 +0800

    解决冲突

--- agentManage/agentManage-common/src/main/java/com/ryx/credit/common/enumc/DictGroup.java
+++ agentManage/agentManage-common/src/main/java/com/ryx/credit/common/enumc/DictGroup.java
@@ -95,7 +95,7 @@ public enum DictGroup {
     LOGISTICS_FAIL_EMAIL("物流失败邮件"),
     REMOVEACCOUNT_FAIL_EMAIL("销账失败邮件"),
     RATYPE("销账方式"),
-    RSTATUS("销账状态");
+    RSTATUS("销账状态"),
 
 
     CERTIFICATION_STATUS("代理商工商认证");
