c62c306fe297228a7ef8be8d791739efa84b9885
sunshengxi <sunshengxi-bj@ruiyinxin.com>
2019-10-21 16:17:50 +0800

    运维，查看，管理

--- agentManage/agentManageAdmin-api/src/main/java/com/ryx/credit/service/agent/AnnounceMentInfoService.java
+++ agentManage/agentManageAdmin-api/src/main/java/com/ryx/credit/service/agent/AnnounceMentInfoService.java
@@ -4,6 +4,7 @@ import com.ryx.credit.common.util.Page;
 import com.ryx.credit.common.util.PageInfo;
 import com.ryx.credit.common.util.ResultVO;
 import com.ryx.credit.pojo.admin.agent.AnnounceMentInfo;
+import com.ryx.credit.pojo.admin.agent.Attachment;
 import com.ryx.credit.pojo.admin.vo.AnnounceMentInfoVo;
 
 import java.util.List;
@@ -19,7 +20,15 @@ public interface  AnnounceMentInfoService {
     ResultVO saveAnn(AnnounceMentInfoVo announceMentInfo);
     ResultVO upStat(AnnounceMentInfo announceMentInfo,String orgStat);
     AnnounceMentInfo queryById(String annId);
+    //公告运维
     PageInfo selectAnnViewsMaintain(Page page,Map map);
+    //公告管理
     PageInfo selectAnnViewsManage(Page page,Map map);
-    PageInfo selectAnnViewxAgent(Page page,Map map,Long userId);
+    //代理商查看公告
+    PageInfo selectAnnViewsAgent(Page page,Map map,Long userId);
+    //非代理商查看公告
+    PageInfo selectAnnViewsRead(Page page,Map map,Long userId);
+
+    //查询附件
+    List<Attachment> queryAttByAnnoid(String id,String busType);
 }
