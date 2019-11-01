48d69d38450e9d080db44ceb6171b99017016f1b
liudh <liudonghui-bj@ruiyinxin.com>
2019-11-01 10:28:30 +0800

    bug修复

--- agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/service/impl/order/OrderActivityServiceImpl.java
+++ agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/service/impl/order/OrderActivityServiceImpl.java
@@ -447,9 +447,14 @@ public class OrderActivityServiceImpl implements OrderActivityService {
             oActivity.setActCode(stringObjectMap.get("ACT_CODE") + "");
             oActivity.setOriginalPrice(new BigDecimal(stringObjectMap.get("ORIGINALPRICE") + ""));
             oActivity.setProductName(stringObjectMap.get("PRO_NAME")+"");
-            oActivity.setPosSpePrice(new BigDecimal(stringObjectMap.get("POS_SPE_PRICE")+""));
-            oActivity.setStandTime(new BigDecimal(stringObjectMap.get("STAND_TIME")+""));
-
+            String posSpePrice = String.valueOf(stringObjectMap.get("POS_SPE_PRICE"));
+            if(StringUtils.isNotBlank(posSpePrice) && !posSpePrice.equals("null") && RegexUtil.checkNum(posSpePrice)){
+                oActivity.setPosSpePrice(new BigDecimal(posSpePrice));
+            }
+            String standTime = String.valueOf(stringObjectMap.get("STAND_TIME"));
+            if(StringUtils.isNotBlank(standTime) && !standTime.equals("null") && RegexUtil.checkNum(standTime)){
+                oActivity.setStandTime(new BigDecimal(standTime));
+            }
             //查询活动是否可见
             OActivity activity = activityMapper.selectByPrimaryKey(oActivity.getId());
             if(StringUtils.isBlank(activity.getVisible())){
