41c1d5e91488537a7bf268e686c9e3c65ce90b4a
chenliang <chenliang-jn@ruiyinxin.com>
2019-11-07 16:01:56 +0800

    wrokid修改

--- agentManage/agentManage-common/src/main/java/com/ryx/credit/common/enumc/DictGroup.java
+++ agentManage/agentManage-common/src/main/java/com/ryx/credit/common/enumc/DictGroup.java
@@ -72,6 +72,7 @@ public enum DictGroup {
     QUIT_MIGR_PLATFORM("代理商退出手刷迁移平台"),
     CAPITAL_MIARKET("保证金变更市场部审批参数"),
     PROFIT_TEMPLATE_APPLY("分润模板线上审批审批流名称"),
+    PROFIT_TEMPLATE_APPLY3("分润模板线上审批审批流名称3"),
     POS("POS审批"),
     POS_APR_BUSNISS("POS审批参数"),
     ORDER_RETURN("订单退货业务选择下一级审批人参数"),
