5a5a62f6fcc65bb232a29d0020d95e61e3420ad9
sunshengxi <sunshengxi-bj@ruiyinxin.com>
2019-11-12 18:56:25 +0800

    机具数量调整申请

--- agentManage/agentManage-common/src/main/java/com/ryx/credit/common/enumc/BusActRelBusType.java
+++ agentManage/agentManage-common/src/main/java/com/ryx/credit/common/enumc/BusActRelBusType.java
@@ -39,6 +39,7 @@ public enum BusActRelBusType {
     thawAgentByCity("/BusActRelBusType/thawAgentByCity","代理商月分润解冻省区申请"),
     thawAgentByBusiness("/BusActRelBusType/thawAgentByBusiness","代理商月分润解冻业务申请"),
     profitTempalteApply("/BusActRelBusType/profitTempalteApply","分润模板线上申请"),
+    orderAdjust("/BusActRelBusType/orderAdj","机具数量调整申请"),
     cardRenew("/BusActRelBusType/cardRenew","物联网卡续费申请");
 
 
