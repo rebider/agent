791ea8d319be04caf4b15bbf0f725a00006a726d
lirongrong <lirongrong@ruiyanxinde.com>
2019-11-13 15:07:29 +0800

    机构管理修复

--- agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/service/impl/order/OrganizationServiceImpl.java
+++ agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/service/impl/order/OrganizationServiceImpl.java
@@ -142,26 +142,18 @@ public class OrganizationServiceImpl implements OrganizationService {
                     if (null != ac.getOrgPlatform() || ac.getOrgPlatform().size() > 0) {
                         List<OrgPlatform> orgPlatform = ac.getOrgPlatform();
                         for (OrgPlatform platform : orgPlatform) {
-                            String[] platCode = platform.getPlatCode().split(",");
-                            String[] platNum = platform.getPlatNum().split(",");
-                            /*if (platCode.length != platNum.length) {
-                                logger.info("请检查业务平台和业务编码填写有误！");
-                                throw new MessageException("请检查业务平台和业务编码填写有误！");
-                            }*/
-                            for (int i = 0; i < platCode.length; i++) {
-                                OrgPlatform orgPlat = new OrgPlatform();
-                                orgPlat.setPlatCode(platCode[i]);
-                                orgPlat.setPlatNum(platNum[i]);
-                                orgPlat.setId(idService.genId(TabId.ORG_PLATFORM));
-                                orgPlat.setOrgId(ac.getOrgId());
-                                orgPlat.setcTime(d);
-                                orgPlat.setcUser(agentVo.getSid());
-                                if (1 != orgPlatformMapper.insertSelective(orgPlat)) {
+                            if(StringUtils.isBlank(platform.getPlatNum())){
+                                throw new MessageException("请填写业务平台号");
+                            }
+                            platform.setId(idService.genId(TabId.ORG_PLATFORM));
+                            platform.setOrgId(ac.getOrgId());
+                            platform.setcTime(d);
+                            platform.setcUser(agentVo.getSid());
+                                if (1 != orgPlatformMapper.insertSelective(platform)) {
                                     logger.info("添加机构平台关系表:{}", "添加机构平台关系表失败");
                                     throw new MessageException("添加机构平台关系表失败");
                                 }
                             }
-                        }
 
                     }
                 } catch (ProcessException e) {
@@ -297,34 +289,23 @@ public class OrganizationServiceImpl implements OrganizationService {
 
                         }
 
-                        //机构平台关系表的添加
+                //机构平台关系表的添加
                 if (null != ac.getOrgPlatform() || ac.getOrgPlatform().size() > 0) {
                     List<OrgPlatform> orgPlatform = ac.getOrgPlatform();
                     for (OrgPlatform platform : orgPlatform) {
-                        String replace = platform.getPlatCode().replace(" ", "#");
-                        String[] platCode =replace.split(",");
-
-                        String[] platNum = platform.getPlatNum().split(",");
+                        if(StringUtils.isBlank(platform.getPlatNum())){
+                            throw new MessageException("请填写业务平台号");
+                        }
                         Date d = Calendar.getInstance().getTime();
-                        for (int i = 0; i < platNum.length; i++) {
-                            OrgPlatform orgPlat = new OrgPlatform();
-                            if (StringUtils.isBlank(platCode[i])||platCode[i].equals("null")){
-                                orgPlat.setPlatCode("#");
-                            }else{
-                                orgPlat.setPlatCode(platCode[i]);
-                            }
-                            orgPlat.setPlatNum(platNum[i]);
-                            orgPlat.setId(idService.genId(TabId.ORG_PLATFORM));
-                            orgPlat.setOrgId(ac.getOrgId());
-                            orgPlat.setcTime(d);
-                            orgPlat.setcUser(agentVo.getSid());
-                            if (1 != orgPlatformMapper.insertSelective(orgPlat)) {
-                                logger.info("添加机构平台关系表:{}", "添加机构平台关系表失败");
-                                throw new MessageException("添加机构平台关系表失败");
-                            }
+                        platform.setId(idService.genId(TabId.ORG_PLATFORM));
+                        platform.setOrgId(ac.getOrgId());
+                        platform.setcTime(d);
+                        platform.setcUser(agentVo.getSid());
+                        if (1 != orgPlatformMapper.insertSelective(platform)) {
+                            logger.info("添加机构平台关系表:{}", "添加机构平台关系表失败");
+                            throw new MessageException("添加机构平台关系表失败");
                         }
                     }
-
                 }
                         //添加新的附件
                         List<String> fileIdList = ac.getOrganizationbleFile();
