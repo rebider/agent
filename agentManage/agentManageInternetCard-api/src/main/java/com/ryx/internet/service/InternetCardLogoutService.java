1016cbdfe3fbfc0666d2a3b6af84f09a6ae0371c
liudh <liudonghui-bj@ruiyinxin.com>
2019-11-21 11:48:30 +0800

    mapper

--- agentManage/agentManageInternetCard-api/src/main/java/com/ryx/internet/service/InternetCardLogoutService.java
+++ agentManage/agentManageInternetCard-api/src/main/java/com/ryx/internet/service/InternetCardLogoutService.java
@@ -1,5 +1,16 @@
 package com.ryx.internet.service;
 
+import com.ryx.credit.common.exception.MessageException;
+import com.ryx.credit.common.result.AgentResult;
+import com.ryx.credit.common.util.Page;
+import com.ryx.credit.common.util.PageInfo;
+import com.ryx.credit.pojo.admin.vo.AgentVo;
+import com.ryx.internet.pojo.InternetLogout;
+import com.ryx.internet.pojo.InternetLogoutDetail;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 /***
  * @Author liudh
  * @Description //TODO 
@@ -8,4 +19,15 @@ package com.ryx.internet.service;
  * @return
  **/
 public interface InternetCardLogoutService {
+
+    PageInfo internetCardLogoutList(InternetLogout internetLogout, Page page, String agentId, Long userId);
+
+    PageInfo internetCardLogoutDetailList(InternetLogoutDetail internetLogoutDetail, Page page, String agentId, Long userId);
+
+    AgentResult saveAndApprove(InternetLogout internetLogout, List<String> iccids, String cUser)throws MessageException;
+
+    AgentResult approvalTask(AgentVo agentVo, String userId) throws Exception;
+
+    AgentResult compressCompensateActivity(String proIns, BigDecimal agStatus)throws Exception;
+
 }
