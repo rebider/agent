c98006f8446b74441e95a851bb9681b100754868
sunshengxi-bj <sunshengxi>
2020-02-17 09:21:19 +0800

    换活动补款明细

--- agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/service/impl/order/OrderOffsetServiceImpl.java
+++ agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/service/impl/order/OrderOffsetServiceImpl.java
@@ -1,15 +1,17 @@
 package com.ryx.credit.service.impl.order;
 
+import com.alibaba.fastjson.JSONObject;
 import com.ryx.credit.common.enumc.*;
 import com.ryx.credit.common.exception.MessageException;
+import com.ryx.credit.common.exception.ProcessException;
 import com.ryx.credit.common.result.AgentResult;
 import com.ryx.credit.commons.utils.StringUtils;
 import com.ryx.credit.dao.CUserMapper;
 import com.ryx.credit.dao.order.*;
 import com.ryx.credit.pojo.admin.order.*;
 import com.ryx.credit.pojo.admin.vo.UserVo;
-import com.ryx.credit.service.ICuserAgentService;
 import com.ryx.credit.service.dict.IdService;
+import com.ryx.credit.service.order.IPaymentDetailService;
 import com.ryx.credit.service.order.OrderOffsetService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,7 +28,6 @@ import static com.ryx.credit.common.enumc.OffsetPaytype.*;
 
 @Service("orderOffsetService")
 public class OrderOffsetServiceImpl implements OrderOffsetService {
-
     @Autowired
     private OPaymentDetailMapper oPaymentDetailMapper;
     @Autowired
@@ -45,6 +46,10 @@ public class OrderOffsetServiceImpl implements OrderOffsetService {
     private CUserMapper cUserMapper;
     @Autowired
     private OReturnOrderMapper oReturnOrderMapper;
+    @Autowired
+    private IPaymentDetailService paymentDetailService;
+    @Autowired
+    private ORefundPriceDiffMapper refundPriceDiffMapper;
 
     private Logger logger = LoggerFactory.getLogger(OrderOffsetServiceImpl.class);
     @Override
@@ -289,6 +294,7 @@ public class OrderOffsetServiceImpl implements OrderOffsetService {
             resultMap.put("offsetPaymentDetails",resPaymentDetail);
             resultMap.put("residueAmt",residue);
         }else if (paytype.equals(THTK.code)){
+            logger.info("退货退款进行抵扣入库");
             //待还金额
             BigDecimal arrearsAmt = BigDecimal.ZERO;
             for(OPaymentDetail oPaymentDetail:opaymentDetailList){
@@ -436,6 +442,86 @@ public class OrderOffsetServiceImpl implements OrderOffsetService {
             }
             resultMap.put("offsetPaymentDetails",resPaymentDetail);
             resultMap.put("residueAmt",residue);
+        }else if (paytype.equals(DDMD.code)){
+            //付款明细
+            List<OPayDetail> oPayDetails = new ArrayList<>();
+            ORefundPriceDiff diff = refundPriceDiffMapper.selectByPrimaryKey(srcId);
+            boolean flag=true;
+            boolean f=true;
+            //1.获取补款实际支付金额
+            BigDecimal residue=amount;
+
+            //多条补款
+            for (OPaymentDetail paymentDetail : opaymentDetailList) {
+                //2.累计还款金额
+                BigDecimal initialize = new BigDecimal(0);
+                if(f==false){
+                    break;
+                }
+                if(residue.compareTo(new BigDecimal(0))==0 ||flag==false){
+                    //如果销账金额已抵扣完销账则停止循环
+                    f=false;
+                    break;
+                }
+                OPayDetail oPayDetail = new OPayDetail();
+                if(residue.compareTo(paymentDetail.getPayAmount())==0){
+                    initialize=paymentDetail.getPayAmount();
+                    flag=false;
+                    logger.info("还款-------:"+initialize);
+                    oPayDetail.setAmount(residue);
+                    oPayDetail.setSrcId(srcId);
+                    residue = residue.subtract(initialize);
+                }else if(residue.compareTo(paymentDetail.getPayAmount())==-1){
+                    initialize.add(residue);
+                    flag=false;
+                    logger.info("还款-------:"+initialize.add(residue));
+                    oPayDetail.setAmount(residue);
+                    oPayDetail.setSrcId(srcId);
+                    residue = BigDecimal.ZERO;
+                }else if(residue.compareTo(paymentDetail.getPayAmount())==1){
+                    residue = residue.subtract(paymentDetail.getPayAmount());
+                    oPayDetail.setAmount(paymentDetail.getPayAmount());
+                    oPayDetail.setSrcId(srcId);
+                    logger.info("还款--------:"+paymentDetail.getPayAmount());
+                }
+                resPaymentDetail.add(paymentDetail);
+                //进行添加付款明细数据
+                oPayDetail.setId(idService.genId(TabId.O_PAY_DETAIL));
+                oPayDetail.setArrId(paymentDetail.getId());
+                oPayDetail.setPayType(paytype);
+                oPayDetail.setBusStat(Status.STATUS_0.status);
+                oPayDetail.setStatus(Status.STATUS_1.status);
+                oPayDetail.setVersion(Status.STATUS_1.status);
+                oPayDetail.setcTm(new Date());
+                oPayDetail.setcUser(diff.getcUser());
+                oPayDetails.add(oPayDetail);
+                if(1!=oPayDetailMapper.insertSelective(oPayDetail)){
+                    logger.info("付款明细添加失败");
+                    throw new MessageException("付款明细添加失败");
+                }
+                paymentDetail.setPaymentStatus(PaymentStatus.FKING.code);
+                if(1!=oPaymentDetailMapper.updateByPrimaryKeySelective(paymentDetail)){
+                    logger.info("付款明细添加失败");
+                    throw new MessageException("付款明细添加失败");
+                }
+
+                if(diff!=null){
+                    if(diff.getMachOweAmt()!=null && diff.getMachOweAmt().compareTo(BigDecimal.ZERO)>0){
+                        logger.info("======错误抵扣=已抵扣{}",JSONObject.toJSONString(diff));
+                        throw new MessageException("已抵扣");
+                    }
+                    if(diff.getMachOweAmt()!=null && diff.getMachOweAmt().compareTo(BigDecimal.ZERO)==0){
+                        logger.info("======抵扣{}",JSONObject.toJSONString(diff));
+                        diff.setMachOweAmt(amount.subtract(residue));
+                        if(1!=refundPriceDiffMapper.updateByPrimaryKeySelective(diff)){
+                            logger.info("======抵扣，更新补差记录失败{}",JSONObject.toJSONString(diff));
+                            throw new MessageException("更新补差记录失败");
+                        }
+                    }
+                }
+            }
+            resultMap.put("offsetPaymentDetails",resPaymentDetail);
+            resultMap.put("residueAmt",residue);
         }
         return AgentResult.okMap(resultMap);
     }
@@ -480,6 +566,8 @@ public class OrderOffsetServiceImpl implements OrderOffsetService {
                     oPaymentDetail.setSrcType(PamentSrcType.TUIKUAN_DIKOU.code);
                 }else if (paytype.equals(FRDK.code)){
                     oPaymentDetail.setSrcType(PamentSrcType.FENRUN_DIKOU.code);
+                }else if (paytype.equals(DDMD.code)){
+                    oPaymentDetail.setSrcType(PamentSrcType.TUICHAJIA_DIKOU.code);
                 }
 
                 oPaymentDetail.setSrcId(srcId);
@@ -567,5 +655,76 @@ public class OrderOffsetServiceImpl implements OrderOffsetService {
         List<OPayDetail> oPayDetails = oPayDetailMapper.selectByExample(oPayDetailExample);
         return oPayDetails;
     }
+    public Map<String, Object> createTkeRecord(OPayment payment, BigDecimal thisPaymentOutstandingAmt) {
+        Map<String, Object> oneTakeoutRecord = new HashMap<>();
+        oneTakeoutRecord.put("orderId", payment.getOrderId());
+        oneTakeoutRecord.put("paymentId", payment.getId());
+        oneTakeoutRecord.put("payType", payment.getPayMethod());
+        oneTakeoutRecord.put("payAmt", thisPaymentOutstandingAmt.setScale(2));
+        oneTakeoutRecord.put("payment", payment);
+        return oneTakeoutRecord;
+    }
 
+    public void updatePaymentDetail(OPaymentDetail updateOPaymentDetail, String srcId, String srcType) throws ProcessException {
+        updateOPaymentDetail.setPaymentStatus(PaymentStatus.JQ.code);
+        updateOPaymentDetail.setRealPayAmount(updateOPaymentDetail.getPayAmount());
+        updateOPaymentDetail.setPayTime(new Date());
+        updateOPaymentDetail.setSrcId(srcId);
+        updateOPaymentDetail.setSrcType(srcType);
+        int counts = oPaymentDetailMapper.updateByPrimaryKeySelective(updateOPaymentDetail);
+        if (counts <= 0) {
+            throw new ProcessException("更新付款明细失败");
+        }
+    }
+
+    public void updatePaymentOutstandingAmt(String paymentId, BigDecimal payAmt) throws ProcessException {
+        OPayment payment = paymentDetailService.getPaymentById(paymentId);
+        payment.setRealAmount(payment.getRealAmount().add(payAmt));
+        payment.setOutstandingAmount(payment.getOutstandingAmount().subtract(payAmt));
+        payment.setPayStatus(PayStatus.PART_PAYMENT.code);
+        int counts = oPaymentMapper.updateByPrimaryKeySelective(payment);
+        if (counts <= 0) {
+            throw new ProcessException("更新订单部分金额支付完成失败");
+        }
+    }
+
+    public void updatePaymentComplete(String paymentId, String srcId, String srcType) throws ProcessException {
+        OPayment payment = paymentDetailService.getPaymentById(paymentId);
+        payment.setRealAmount(payment.getPayAmount());
+        payment.setOutstandingAmount(BigDecimal.ZERO);
+        payment.setPayCompletTime(new Date());
+        payment.setPayStatus(PayStatus.CLOSED.code);
+        payment.setPlanSucTime(new Date());
+        int counts = oPaymentMapper.updateByPrimaryKeySelective(payment);
+        if (counts <= 0) {
+            throw new ProcessException("更新订单全部付款记录为完成");
+        }
+    }
+    public void insertToPaymentDetail(String paymentId, OPaymentDetail paymentDetail, BigDecimal thisDetailPayAmt, String agentId, String srcId, String srcType, String userid) throws ProcessException {
+        try {
+            OPaymentDetail newDeatil = new OPaymentDetail();
+            newDeatil.setId(idService.genId(TabId.o_payment_detail));
+            newDeatil.setPaymentId(paymentId);
+            newDeatil.setPaymentType(paymentDetail.getPaymentType());
+            newDeatil.setOrderId(paymentDetail.getOrderId());
+            newDeatil.setPayType(paymentDetail.getPayType());
+            newDeatil.setPayAmount(thisDetailPayAmt);
+            newDeatil.setRealPayAmount(thisDetailPayAmt);
+            newDeatil.setPayTime(new Date());
+            newDeatil.setAgentId(agentId);
+            newDeatil.setSrcId(srcId);
+            newDeatil.setSrcType(srcType);
+            newDeatil.setPaymentStatus(PaymentStatus.JQ.code);
+            newDeatil.setcDate(new Date());
+            newDeatil.setcUser(userid);
+            newDeatil.setPlanNum(BigDecimal.ZERO);
+            newDeatil.setPlanPayTime(new Date());
+            if(1!=oPaymentDetailMapper.insertSelective(newDeatil)){
+                throw new MessageException("添加付款明细异常");
+            }
+        } catch (Exception e) {
+            logger.error("插入抵扣付款明细失败srcId={" + srcId + "},srcType{" + srcType + "}", e);
+            throw new ProcessException("插入抵扣付款明细失败srcId={" + srcId + "},srcType{" + srcType + "}");
+        }
+    }
 }
