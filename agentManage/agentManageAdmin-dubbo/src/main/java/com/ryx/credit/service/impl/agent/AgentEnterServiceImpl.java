4657a3bd677515cd5b4ef87887a929a3abb3d362
cx <chenxiaoxiang@ruiyanxinde.com>
2020-03-17 14:28:19 +0800

    打款金额判断

--- agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/service/impl/agent/AgentEnterServiceImpl.java
+++ agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/service/impl/agent/AgentEnterServiceImpl.java
@@ -1150,25 +1150,9 @@ public class AgentEnterServiceImpl implements AgentEnterService {
                 throw new ProcessException("新增资金流水失败");
             }
         }
-        //入网程序调用
-//        try {
-//            ImportAgent importAgent = new ImportAgent();
-//            importAgent.setDataid(busId);
-//            importAgent.setDatatype(AgImportType.NETINAPP.name());
-//            importAgent.setBatchcode(processingId);
-//            importAgent.setcUser(rel.getcUser());
-//            if (1 != aimportService.insertAgentImportData(importAgent)) {
-//                logger.info("代理商审批通过-添加开户任务失败");
-//            } else {
-//                logger.info("代理商审批通过-添加开户任务成功!{},{}", AgImportType.NETINAPP.getValue(), busId);
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        } finally {
-        //todo 生成后台用户
+
         agentService.createBackUserbyAgent(agent.getId());
         agentNetInNotityService.asynNotifyPlatform(busId,NotifyType.NetInAdd.getValue());
-//        }
 
         return ResultVO.success(null);
     }
