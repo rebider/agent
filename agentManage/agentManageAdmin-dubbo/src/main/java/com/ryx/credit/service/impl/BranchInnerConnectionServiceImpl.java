e270aa9b7cc60ab137259d745399f00a0be62567
zhaoyade-bj <zhaoyade-bj@ruiyinxin.com>
2020-03-18 17:15:47 +0800

    优化

--- agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/service/impl/BranchInnerConnectionServiceImpl.java
+++ agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/service/impl/BranchInnerConnectionServiceImpl.java
@@ -128,7 +128,13 @@ public class BranchInnerConnectionServiceImpl implements IBranchInnerConnectionS
         }
 
         //查询busNum
-        List<String> busNums = agentBusInfoMapper.selectBusNumByBusProCode(FastMap.fastMap("branchLogin", branch));
+        List<String> busNums = agentBusInfoMapper.selectBusNumByBusProCode(
+                FastMap.fastMap("platformTypes", new String[]{PlatformType.POS.code,
+                        PlatformType.ZPOS.code,
+                        PlatformType.ZHPOS.code,
+                        PlatformType.SSPOS.code})
+                        .putKeyV("branchLogin", branch)
+        );
         if (busNums.size() > 0) {
             JSONObject data = new JSONObject();
             data.put("addAccounts", inner);
@@ -521,7 +527,13 @@ public class BranchInnerConnectionServiceImpl implements IBranchInnerConnectionS
         CBranchInner cBranchInner = branchInnerMapper.selectByPrimaryKey(id);
 
         //查询busNum
-        List<String> busNums = agentBusInfoMapper.selectBusNumByBusProCode(FastMap.fastMap("branchLogin", cBranchInner.getBranchLogin()));
+        List<String> busNums = agentBusInfoMapper.selectBusNumByBusProCode(
+                FastMap.fastMap("platformTypes", new String[]{PlatformType.POS.code,
+                        PlatformType.ZPOS.code,
+                        PlatformType.ZHPOS.code,
+                        PlatformType.SSPOS.code})
+                        .putKeyV("branchLogin", cBranchInner.getBranchLogin())
+        );
         if (busNums.size() > 0) {
             JSONObject data = new JSONObject();
             data.put("addAccounts", cBranchInner.getInnerLogin());
