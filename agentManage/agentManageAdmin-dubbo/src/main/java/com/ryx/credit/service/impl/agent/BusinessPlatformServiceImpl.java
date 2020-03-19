d16fc13bcbd5426a1255f5a5ea9a5d77615cadd8
zhaoyade-bj <zhaoyade-bj@ruiyinxin.com>
2020-03-19 16:03:35 +0800

    优化

--- agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/service/impl/agent/BusinessPlatformServiceImpl.java
+++ agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/service/impl/agent/BusinessPlatformServiceImpl.java
@@ -777,18 +777,30 @@ public class BusinessPlatformServiceImpl implements BusinessPlatformService {
                         List<String> oldAccounts = lmsUserService.queryByBusNum(agentInner.getBusNum());
                         //查询新省区，对应的大区经理账号
                         List<String> newAccounts = branchInnerMapper.selectInnerLoginByBranch(agentBusInfoVo.getAgDocPro());
-                        //调接口修改
-                        //branchInnerConnectionServiceImpl.
 
-                        JSONObject data = new JSONObject();
-                        data.put("addAccounts", String.join(",", newAccounts));
-                        data.put("delAccounts", String.join(",", oldAccounts));
-                        data.put("orgId", agentInner.getBusNum());
-                        try {
-                            AgentResult agentResult = request("ORG021", data);
-                        } catch (Exception e) {
-                            e.printStackTrace();
+                        if (newAccounts.size() > 0) {
+                            JSONObject data = new JSONObject();
+                            data.put("addAccounts", String.join(",", newAccounts));
+                            data.put("delAccounts", String.join(",", oldAccounts));
+                            data.put("orgId", agentInner.getBusNum());
+                            try {
+                                AgentResult agentResult = request("ORG021", data);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        } else if (newAccounts.size() <= 0 && oldAccounts.size() > 0) {
+                            JSONObject data = new JSONObject();
+                            data.put("loginname", String.join(",", oldAccounts));
+                            data.put("dType", "1");
+                            data.put("delOrgIds", agentInner.getBusNum());
+                            try {
+                                AgentResult agentResult = request("ORG020", data);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
                         }
+
+
                     }
                 }
             }
