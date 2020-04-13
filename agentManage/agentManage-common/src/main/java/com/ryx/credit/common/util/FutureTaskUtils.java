438a56f1974ca894523eaabfca715f39629656fb
chenliang <chenliang-jn@ruiyinxin.com>
2020-04-13 09:09:00 +0800

    分润导入问题解决

--- agentManage/agentManage-common/src/main/java/com/ryx/credit/common/util/FutureTaskUtils.java
+++ agentManage/agentManage-common/src/main/java/com/ryx/credit/common/util/FutureTaskUtils.java
@@ -9,28 +9,46 @@ import java.util.concurrent.FutureTask;
 
 public class FutureTaskUtils {
 
-    public static Map<String, String> getTaskResult(List<FutureTask<Map<String, String>>> taskList, List<String> taskNameList, Logger logger) {
-        Map<String, String> result = new HashMap<>();
-        Boolean success = Boolean.TRUE;
+    public static Map<String, Object> getTaskResult(List<FutureTask<Map<String, Object>>> taskList, List<String> taskNameList, Logger logger) {
+        logger.info(taskNameList + "开始执行");
+        Map<String, Object> result = new HashMap<>();
         for (int i = 0; i < taskList.size(); i++) {
-            FutureTask<Map<String, String>> thread = taskList.get(i);
-
+            FutureTask<Map<String, Object>> thread = taskList.get(i);
             try {
-                Map<String, String> sheetMap = thread.get();
+                Map<String, Object> sheetMap = thread.get();
                 if (sheetMap.get("result").equals("success")) {
+                    result.put("result", "success");
                     continue;
-                } else if(sheetMap.get("result").equals("fail")){
-                    result.put("result","fail");
-                    result.put("Err",sheetMap.get("Err"));
-                    return sheetMap;
+                } else if (sheetMap.get("result").equals("fail")) {
+                    result.put("result", "fail");
+                    result.put("Err", sheetMap.get("Err"));
+                    return result;
+                } else if (sheetMap.get("result").equals("dispose")) {
+                    for (int j = 0; j < taskList.size(); j++) {
+                        FutureTask<Map<String, Object>> FutureTaskthread = taskList.get(j);
+                        Map<String, Object> FutureTaskSheetMap = FutureTaskthread.get();
+                        if (FutureTaskSheetMap.get("result").equals("dispose")) {
+                            List<Map<String, Object>> resultList = (List<Map<String, Object>>) FutureTaskSheetMap.get("disposeResult");
+                            if (resultList != null && resultList.size() != 0) {
+                                if (result != null && result.size() != 0) {
+                                    resultList.addAll((List<Map<String, Object>>) result.get("disposeResult"));
+                                }
+                            }
+                            result.put("result", "dispose");
+                            result.put("disposeResult"+taskNameList.get(0), resultList);
+                        } else if (FutureTaskSheetMap.get("result").equals("fail")) {
+                            result.put("result", "fail");
+                            result.put("Err", sheetMap.get("Err"));
+                            return result;
+                        }
+                    }
+                    return result;
                 }
             } catch (Exception e) {
                 e.printStackTrace();
             }
-
-            result.put("result","success");
-		}
-		return result;
-	}
+        }
+        return result;
+    }
 
 }
