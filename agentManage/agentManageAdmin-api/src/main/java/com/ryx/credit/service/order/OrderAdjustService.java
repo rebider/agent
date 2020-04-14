11e6cee89983a1da1ddd3fe50a3582a32a524a27
sunshengxi-bj <sunshengxi>
2020-04-14 14:31:33 +0800

    分离订单调整的service

---
+++ agentManage/agentManageAdmin-api/src/main/java/com/ryx/credit/service/order/OrderAdjustService.java
@@ -0,0 +1,87 @@
+package com.ryx.credit.service.order;
+
+import com.ryx.credit.common.exception.ProcessException;
+import com.ryx.credit.common.result.AgentResult;
+import com.ryx.credit.common.util.Page;
+import com.ryx.credit.common.util.PageInfo;
+import com.ryx.credit.pojo.admin.order.OrderAdj;
+import com.ryx.credit.pojo.admin.vo.OrderAdjustVo;
+import com.ryx.credit.pojo.admin.vo.OrderUpModelVo;
+
+import java.util.List;
+import java.util.Map;
+
+public interface OrderAdjustService {
+    /**
+     * 查询欠款信息
+     * @param orderId
+     * @return
+     */
+    AgentResult refreshPaymentDetail(String orderId);
+
+
+    AgentResult saveAdjInfo(OrderUpModelVo orderUpModelVo, Map map) throws Exception;
+
+    PageInfo queryAgentUpModelList(Map par, Page page);
+
+    AgentResult loadUpModelInfo(String adjId);
+
+    /**
+     * 启动订单调整审批
+     * @return
+     * @throws Exception
+     */
+    AgentResult startOrderAdjust(String id, String cuser) throws Exception;
+
+    /**
+     * 修改订单调整数据
+     * @param orderUpModelVo
+     * @param userId
+     * @return
+     * @throws Exception
+     */
+    AgentResult updateOrderAdjust(OrderUpModelVo orderUpModelVo, String userId) throws Exception;
+
+    OrderAdj getByAdjIdStatus(String adjId);
+
+    /**
+     * 订单调整审批处理
+     * @param orderUpModelVo
+     * @param userId
+     * @return
+     * @throws Exception
+     */
+    AgentResult approvalTaskOrderAdjust(OrderUpModelVo orderUpModelVo, String userId) throws Exception;
+
+    /**
+     * 订单调整审批通过
+     * @param insid
+     * @param actname
+     * @return
+     */
+    AgentResult approveFinishOrderAdjust(String insid, String actname) throws Exception;
+
+    AgentResult approvalTaskSettle(OrderAdj orderAdj) throws ProcessException;
+
+    Map<String,Object> freshRefundAmo(String adjId,String proAmount,String refundAmo,String refundType,String takeAmt);
+
+    Map<String,Object> saveProAmo(String adjId,String proAmount);
+
+    /**
+     * 订单数量调整导出
+     */
+    List<OrderAdjustVo> excelOrderAdjustAll(Map map);
+
+    /**
+     * 查看全部调整明细
+     * @param par
+     * @param page
+     * @return
+     */
+    PageInfo queryUpModelListAll(Map par, Page page);
+
+    /**
+     * 执行订单调整计划
+     */
+    AgentResult adjustDoPayPlan(String adjId,OrderAdj orderAdj)throws Exception;
+}
