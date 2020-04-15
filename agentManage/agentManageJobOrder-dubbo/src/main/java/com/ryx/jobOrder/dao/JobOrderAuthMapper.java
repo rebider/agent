6cb9f83c54536bb70519a5f06c8ddfadef0dc20e
mlf2020 <mlf2020@163.com>
2020-04-15 22:13:03 +0800

    工单系统增加 代理商权限

--- agentManage/agentManageJobOrder-dubbo/src/main/java/com/ryx/jobOrder/dao/JobOrderAuthMapper.java
+++ agentManage/agentManageJobOrder-dubbo/src/main/java/com/ryx/jobOrder/dao/JobOrderAuthMapper.java
@@ -9,6 +9,7 @@ public interface JobOrderAuthMapper {
     List<Map<String,Object>> getReqJobOrderAuth(@Param("userId") Long userId);
     Map<String,Object> getAcceptCode(@Param("jobOrderType") String jobOrderType);
     Map<String,Object> getAcceptGroup(@Param("userId") String userId);
+    Map<String,Object> getAcceptGroupAgent();
     List<Map<String,Object>> getAllAcceptGroup();
     Map<String,Object> getAcceptByAcceptCode(@Param("acceptCode") String acceptCode);
     Map<String,Object> getAcceptByuserid(@Param("userId") String userID);
