0c13390e293570512bfa8b41021111bd1328aa17
chenqiutian <chenqiutian-yt@ruiyinxin.com>
2020-04-17 16:38:17 +0800

    导入记录查询优化

--- agentManage/agentManage-common/src/main/java/com/ryx/credit/common/enumc/ProfitDataImportType.java
+++ agentManage/agentManage-common/src/main/java/com/ryx/credit/common/enumc/ProfitDataImportType.java
@@ -5,6 +5,9 @@ package com.ryx.credit.common.enumc;
 public enum ProfitDataImportType {
 
     FRSJDR("00","分润数据导入"),
+    FRYCDR("77","分润第一次导入"),
+    FRECDR("88","分润第二次导入"),
+    FRBCKDR("99","分润第三次补出款导入"),
     FXSJDR("01","返现数据导入");
 
     public String key;
