0d7f667fca23749decaf4fdb699676199600b3f7
lirongrong <lirongrong@ruiyanxinde.com>
2020-05-07 16:13:11 +0800

    增加过滤条件

--- agentManage/agentManage-common/src/main/java/com/ryx/credit/common/enumc/ReceiptPlanExportColum.java
+++ agentManage/agentManage-common/src/main/java/com/ryx/credit/common/enumc/ReceiptPlanExportColum.java
@@ -16,8 +16,8 @@ public enum ReceiptPlanExportColum {
     ReceiptPlan_ViewTitle("排单日期,订单号,订单日期,代理商唯一编码,自编唯一码,代理商,平台,机具类型,活动名称,订货厂家,机型,总数量,排单数量,剩余未排单数量,收货人,收货人地址,收货人联系方式,配货时间","排单导出标题"),
 
     //导出欠款记录
-    ARREARAGE_Column("TM,AG_UNIQ_NUM,AG_NAME,BUS_NUM,PLATFORM_NAME,JJQK","欠款导出字段"),
-    ARREARAGE_Title("月份,代理商唯一编码,代理商名称,业务平台编码,业务平台,机具欠款","欠款导出标题"),
+    ARREARAGE_Column("TM,AGID,AG_UNIQ_NUM,AG_NAME,BUS_NUM,PLATFORM_NAME,JJQK","欠款导出字段"),
+    ARREARAGE_Title("月份,代理商唯一编码,自编唯一码,代理商名称,业务平台编码,业务平台,机具欠款","欠款导出标题"),
 
     //导出活动调整
     RefundPriceDiff_Column("ID,AGENT_ID,AG_NAME,DEDUCT_AMT,BELOW_PAY_AMT,SHARE_DEDUCT_AMT,GATHER_TIME,GATHER_AMT,APPLY_REMARK,REVIEW_STATUS,ORDER_TYPE,S_TIME,U_TIME","活动调整导出字段"),
