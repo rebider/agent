ac6aea534f1ec024aa709567af83cdf4df7c0777
lhl <lihaili-bj@ruiyinxin.com>
2020-05-28 17:13:54 +0800

    代理商信息查询接口；AG码账号状态查询接口；

--- agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/dao/agent/AgentMapper.java
+++ agentManage/agentManageAdmin-dubbo/src/main/java/com/ryx/credit/dao/agent/AgentMapper.java
@@ -68,4 +68,8 @@ public interface AgentMapper {
     int queryAgentRiskCount(@Param("map") Map<String, Object> map);
     List<Map<String, Object>> queryAgentRiskView(@Param("map") Map<String, Object> map,@Param("page") Page page);
     List<AgentoutVo> exportAgentRisk(@Param("map") Map<String, Object> map);
+
+    List<Map<String, Object>> queryAgentColinfoByAgentId(Map<String, Object> reqMap);
+
+    List<Map<String, Object>> queryAgentStatusByAgentId(Map<String, Object> reqMap);
 }
