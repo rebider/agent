1a35308fe0908f79f2d1367e2457f34b54eb7fed
cx <chenxiaoxiang@ruiyanxinde.com>
2020-06-10 09:44:57 +0800

    审批流任务调整

--- agentManage/agentManageActivity-dubbo/src/main/java/com/ryx/credit/activity/task/AppyChangeCardExecutionListener.java
+++ agentManage/agentManageActivity-dubbo/src/main/java/com/ryx/credit/activity/task/AppyChangeCardExecutionListener.java
@@ -8,9 +8,10 @@ import org.activiti.engine.delegate.TaskListener;
 /**
  * 作者：cx
  * 时间：2020/6/8
- * 描述：
+ * 描述：结算卡变更2.0流程执行监听器,任务监听器
  */
-public class AppyChangeCardExecutionListener extends BaseTaskListener  implements TaskListener, ExecutionListener {
+public class AppyChangeCardExecutionListener  extends BaseTaskListener implements TaskListener,  ExecutionListener {
+
     @Override
     public void notify(DelegateExecution delegateExecution) throws Exception {
 
