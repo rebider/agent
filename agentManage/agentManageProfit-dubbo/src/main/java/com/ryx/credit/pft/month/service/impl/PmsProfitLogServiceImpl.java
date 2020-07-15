29b870ec49bd34ce3421cd5d5936865e3cbeaaed
chenliang <chenliang-jn@ruiyinxin.com>
2020-07-15 15:21:17 +0800

    月分润去除buscode非空约束，取消默认值#

--- agentManage/agentManageProfit-dubbo/src/main/java/com/ryx/credit/pft/month/service/impl/PmsProfitLogServiceImpl.java
+++ agentManage/agentManageProfit-dubbo/src/main/java/com/ryx/credit/pft/month/service/impl/PmsProfitLogServiceImpl.java
@@ -680,7 +680,6 @@ public class PmsProfitLogServiceImpl implements IPmsProfitLogService {
             pmsProfitTempWithBLOBs.setMonth(pmsProfitLog.getMonth());
             pmsProfitTempWithBLOBs.setUniqueFlag((list.get(i).get("Cell0")));
             pmsProfitTempWithBLOBs.setAgentName((list.get(i).get("Cell1")));
-            pmsProfitTempWithBLOBs.setBusCode("#");
             pmsProfitTempWithBLOBs.setSheetHead(callMapToXML(listOne.get(0)));
             pmsProfitTempWithBLOBs.setSheetData(callMapToXML(list.get(i)));
             pmsProfitTempWithBLOBs.setSheetName(sheetName);
@@ -705,15 +704,12 @@ public class PmsProfitLogServiceImpl implements IPmsProfitLogService {
             pf.setUpdateTime(pf.getImportTime());
             pf.setOrderNumber(new BigDecimal((list.get(i).get("rowNum"))));
             pf.setImportBatch(pmsProfitLog.getBatchNo());
-            pf.setBusCode("#");
+
 
             String agentId = list.get(i).get("Cell0").trim();
             String orgID =null;
             if ("汇总".equals(sheetName)) {
                 pmsProfitTempWithBLOBs.setBusCode(list.get(i).get("Cell3"));
-                if(pmsProfitTempWithBLOBs.getBusCode() ==null||"".equals(pmsProfitTempWithBLOBs.getBusCode())){
-                    pmsProfitTempWithBLOBs.setBusCode("#");
-                }
                  orgID = list.get(i).get("Cell8").trim();
 
                 if (null != list.get(i).get("Cell4") && !"".equals(list.get(i).get("Cell4"))) {
